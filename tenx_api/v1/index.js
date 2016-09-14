7593279625864a49885d96529efd79f94f7802a4
zhangpc <zhangpc@tenxcloud.com>
2016-09-14 18:56:15 +0800

    add tenx_api part 1

---
+++ tenx_api/v1/index.js
@@ -0,0 +1,10 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2016 TenxCloud. All Rights Reserved.
+ * 
+ * Tenxcloud api index
+ * 
+ * v0.1 - 2016-09-12
+ * @author Zhangpc
+ */
+exports.k8s = require('./apis/cicd')
