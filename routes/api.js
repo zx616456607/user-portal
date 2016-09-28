c0a97a62d06d75dbea653229d0d1ddfced509096
zhangpc <zhangpc@tenxcloud.com>
2016-09-28 16:02:45 +0800

    add all app backend api

--- routes/api.js
+++ routes/api.js
@@ -29,17 +29,40 @@ module.exports = function (Router) {
   router.get('/storage-pools/:pool/volumes', storageController.getStorageDetail)
 
   // Apps 
-  router.get('/clusters/:master/apps', appController.getApps)
-  router.get('/clusters/:master/apps/:app_name/services', appController.getAppServices)
-  
+  router.post('/clusters/:cluster/apps', appController.createApp)
+  router.get('/clusters/:cluster/apps', appController.getApps)
+  router.put('/clusters/:cluster/apps/batch-delete', appController.deleteApps)
+  router.put('/clusters/:cluster/apps/batch-stop', appController.stopApps)
+  router.put('/clusters/:cluster/apps/batch-start', appController.startApps)
+  router.put('/clusters/:cluster/apps/batch-restart', appController.restartApps)
+  router.put('/clusters/:cluster/apps/batch-status', appController.getAppsStatus)
+  router.get('/clusters/:cluster/apps/:app_name/services', appController.getAppServices)
+  router.post('/clusters/:cluster/apps/:app_name/services', appController.addService)
+  router.put('/clusters/:cluster/apps/:app_name/services/batch-delete', appController.deleteServices)
+  router.get('/clusters/:cluster/apps/:app_name/orchfile', appController.getAppOrchfile)
+  // spi
+  router.get('/clusters/:cluster/apps/:app_name/logs', appController.getAppLogs)
+
   // Services
-  router.get('/clusters/:master/services/:service_name/containers', serviceController.getServiceContainers)
+  router.put('/clusters/:cluster/services/batch-start', serviceController.startServices)
+  router.put('/clusters/:cluster/services/batch-stop', serviceController.stopServices)
+  router.put('/clusters/:cluster/services/batch-restart', serviceController.restartServices)
+  router.get('/clusters/:cluster/services/batch-status', serviceController.getServicesStatus)
+  router.get('/clusters/:cluster/services/:service_name/detail', serviceController.getServiceDetail)
+  router.get('/clusters/:cluster/services/:service_name/containers', serviceController.getServiceContainers)
+  router.put('/clusters/:cluster/services/:service_name/manualscale', serviceController.manualScaleService)
+  router.put('/clusters/:cluster/services/:service_name/autoscale', serviceController.autoScaleService)
+  router.put('/clusters/:cluster/services/:service_name/quota', serviceController.changeServiceQuota)
+  router.put('/clusters/:cluster/services/:service_name/ha', serviceController.changeServiceHa)
+  // spi
+  router.post('/clusters/:cluster/services/:service_name/domain', serviceController.bindServiceDomain)
 
   // Containers
-  router.get('/clusters/:master/containers', containerController.getContainers)
+  router.get('/clusters/:cluster/containers', containerController.getContainers)
+  router.get('/clusters/:cluster/containers/:container_name/detail', containerController.getContainerDetail)
   
-  //Configs
-  router.get('/clusters/:master/configs',configController.getConfigGroup)
+  // Configs
+  router.get('/clusters/:cluster/configs',configController.getConfigGroup)
   
   return router.routes()
 }
