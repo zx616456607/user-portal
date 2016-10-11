b59cecb15da771a815c1d60475dec2955b3e6607
baiyu <baiyu@tenxcloud.com>
2016-10-11 19:13:05 +0800

    add start one databate and cache

--- routes/api.js
+++ routes/api.js
@@ -63,7 +63,8 @@ module.exports = function (Router) {
   router.get('/clusters/:cluster/containers/:container_name/detail', containerController.getContainerDetail)
   
   // Configs
-  router.get('/clusters/:cluster/configs',configController.getConfigGroup)
+  router.get('/clusters/:cluster/configgroups',configController.getConfigGroup)
+  router.get('/clusters/:cluster/configgroups/:name',configController.getConfigGroupName)
   router.post('/clusters/:cluster/configs',configController.createConfigGroup)
   router.post('/clusters/:cluster/configs/delete',configController.deleteConfigGroup)
 
