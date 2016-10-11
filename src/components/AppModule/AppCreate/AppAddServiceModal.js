e586aea6a672c9486c459e0d4239e76b65805eb9
zhangpc <zhangpc@tenxcloud.com>
2016-10-11 18:04:26 +0800

    fix issue

--- src/components/AppModule/AppCreate/AppAddServiceModal.js
+++ src/components/AppModule/AppCreate/AppAddServiceModal.js
@@ -159,7 +159,7 @@ function mapStateToProps(state, props) {
 
   return {
     registry,
-		registryServer: registry,
+		registryServer: server,
     publicImageList: imageList,
     isFetching
   }
