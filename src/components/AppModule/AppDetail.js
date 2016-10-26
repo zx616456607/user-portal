a9349b16fd98a80c9000e9212482a02f87937808
zhangsh <shouhong@shouhongmatoMacBook-Pro.local>
2016-10-26 16:56:08 +0800

    fix merge error

--- src/components/AppModule/AppDetail.js
+++ src/components/AppModule/AppDetail.js
@@ -148,7 +148,6 @@ class AppDetail extends Component {
                 </TabPane>
                 <TabPane tab="应用拓扑图" key="#topology">应用拓扑图</TabPane>
                 <TabPane tab="编排文件" key="#stack" ><AppGraph key="AppGraph" cluster={this.props.cluster} appName={this.props.appName} /></TabPane>
-                <TabPane tab="编排文件" key="#stack" ><AppGraph key="AppGraph" /></TabPane>
                 <TabPane tab="操作日志" key="#logs" >
                   <AppLog key="AppLog"
                     cluster={this.props.cluster}
