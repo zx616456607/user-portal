571f65b01dc78ac1319bb1bad58fed9cead5d620
zhaoxy <312364705@qq.com>
2016-11-03 14:24:13 +0800

    系统设置/成员管理(除却详情页/查询未找到)

--- src/components/SettingModal/MemberManage/index.js
+++ src/components/SettingModal/MemberManage/index.js
@@ -9,19 +9,25 @@
  */
 import React,{ Component } from 'react'
 import './style/MemberManage.less'
-import { Row, Col, Button, Input, Select, Card, Icon, Table } from 'antd'
+import { Row, Col, Button, Input, Select, Card, Icon, Table, Modal, Form, Checkbox, Tooltip, } from 'antd'
 
 const Option = Select.Option
+const createForm = Form.create;
+const FormItem = Form.Item;
+
+function noop() {
+  return false;
+}
 
 const data = [
-  {key: '1',name: 'a',tel: 11,email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: 5,},
-  {key: '2',name: 'ba',tel: 12,email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: 5,},
-  {key: '3',name: 'caa',tel: 13,email: 'emaila@tenxcloud.com',style: '普通成员',team: '1',rest: 5,},
-  {key: '4',name: 'daaa',tel: 14,email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: 5,},
-  {key: '5',name: 'eaaaa',tel: 15,email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: 5,},
-  {key: '6',name: 'f',tel: 16,email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: 5,},
-  {key: '7',name: 'g',tel: 17,email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: 5,},
-  {key: '8',name: 'h',tel: 18,email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: 5,},
+  {key: '1',name: 'a',tel: '11',email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: '5',},
+  {key: '2',name: 'ba',tel: '12',email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: '5',},
+  {key: '3',name: 'caa',tel: '13',email: 'emaila@tenxcloud.com',style: '普通成员',team: '1',rest: '5',},
+  {key: '4',name: 'daaa',tel: '14',email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: '5',},
+  {key: '5',name: 'eaaaa',tel: '15',email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: '5',},
+  {key: '6',name: 'f',tel: '16',email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: '5',},
+  {key: '7',name: 'g',tel: '17',email: 'emaila@tenxcloud.com',style: '普通成员',team: '2',rest: '5',},
+  {key: '8',name: 'h',tel: '18',email: 'emaila@tenxcloud.com',style: '团队管理员',team: '1',rest: '5',},
   ];
 
 let MemberTable =  React.createClass({
@@ -40,8 +46,20 @@ let MemberTable =  React.createClass({
   render() {
     let { sortedInfo, filteredInfo } = this.state
     const { searchResult, notFound } = this.props.scope.state
-    sortedInfo = sortedInfo || {};
-    filteredInfo = filteredInfo || {};
+    sortedInfo = sortedInfo || {}
+    filteredInfo = filteredInfo || {}
+    const pagination = {
+      total: data.length,
+      showSizeChanger: true,
+      defaultPageSize: 5,
+      pageSizeOptions: ['5','10','15','20'],
+      onShowSizeChange(current, pageSize) {
+        console.log('Current: ', current, '; PageSize: ', pageSize);
+      },
+      onChange(current) {
+        console.log('Current: ', current);
+      },
+    }
     const columns = [
       {
         title: '成员名',
@@ -49,16 +67,20 @@ let MemberTable =  React.createClass({
         key: 'name',
         sorter: (a, b) => a.name.length - b.name.length,
         sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
+        className: 'memberName',
+        width: 150,
       },
       {
         title: '手机',
         dataIndex: 'tel',
         key: 'tel',
+        width: 150,
       },
       {
         title: '邮箱',
         dataIndex: 'email',
         key: 'email',
+        width: 200,
       },
       {
         title: '类型',
@@ -70,21 +92,25 @@ let MemberTable =  React.createClass({
         ],
         filteredValue: filteredInfo.style,
         onFilter: (value, record) => record.style.indexOf(value) === 0,
+        width: 150,
       },
       {
         title: '团队',
         dataIndex: 'team',
         key: 'team',
+        width: 150,
       },
       {
         title: '余额',
         dataIndex: 'rest',
         key: 'rest',
+        width: 150,
       },
       {
         title: '操作',
         dataIndex: 'operation',
         key: 'operation',
+        width: 180,
         render: (text, record) => (
           <div>
             <Button icon="setting" className="setBtn">管理</Button>
@@ -94,17 +120,197 @@ let MemberTable =  React.createClass({
       },]
     if(notFound){
       return (
-        <div>没有结果</div>
+        <div>没有找到</div>
       )
     } else {
       return (
               <Table columns={columns}
                      dataSource={searchResult.length === 0?data : searchResult}
+                     pagination={pagination}
                      onChange={this.handleChange} />
       )
     }
   },
 })
+let NewMemberForm = React.createClass({
+  userExists(rule, value, callback) {
+    if (!value) {
+      callback();
+    } else {
+      setTimeout(() => {
+        if (value === 'zhaoxueyu') {
+          callback([new Error('抱歉，该用户名已被占用。')]);
+        } else {
+          callback();
+        }
+      }, 800);
+    }
+  },
+  checkPass(rule, value, callback) {
+    const { validateFields } = this.props.form;
+    if (value) {
+      validateFields(['rePasswd'], { force: true });
+    }
+    callback();
+  },
+  checkPass2(rule, value, callback) {
+    const { getFieldValue } = this.props.form;
+    if (value && value !== getFieldValue('passwd')) {
+      callback('两次输入密码不一致！');
+    } else {
+      callback();
+    }
+  },
+  handleOk() {
+    const { scope } = this.props
+    this.props.form.validateFields((errors, values) => {
+      if (!!errors) {
+        return;
+      }
+      scope.setState({
+        visible: false,
+      })
+    });
+  },
+  handleCancel(e) {
+    const { scope } = this.props
+    e.preventDefault();
+    this.props.form.resetFields();
+    scope.setState({
+      visible: false,
+    })
+  },
+  render() {
+    const { form, scope, visible } = this.props
+    const { getFieldProps, getFieldError, isFieldValidating } = form
+    const text = <span>前台只能添加普通成员</span>
+    const nameProps = getFieldProps('name', {
+      rules: [
+        { required: true, min: 5, message: '用户名至少为 5 个字符' },
+        { validator: this.userExists },
+      ],
+    })
+    const telProps = getFieldProps('tel', {
+      validate: [{
+        rules: [
+          { required: true, message: '请输入手机号' },
+        ],
+        trigger: 'onBlur',
+      }, {
+        rules: [
+          { type: 'email', message: '请输入正确的邮箱地址' },
+        ],
+        trigger: ['onBlur', 'onChange'],
+      }],
+    });
+    const emailProps = getFieldProps('email', {
+      validate: [{
+        rules: [
+          { required: true, message: '请输入邮箱地址' },
+        ],
+        trigger: 'onBlur',
+      }, {
+        rules: [
+          { type: 'email', message: '请输入正确的邮箱地址' },
+        ],
+        trigger: ['onBlur', 'onChange'],
+      }],
+    })
+    const passwdProps = getFieldProps('passwd', {
+      rules: [
+        { required: true, whitespace: true, message: '请填写密码' },
+        { validator: this.checkPass },
+      ],
+    });
+    const rePasswdProps = getFieldProps('rePasswd', {
+      rules: [{
+        required: true,
+        whitespace: true,
+        message: '请再次输入密码',
+      }, {
+        validator: this.checkPass2,
+      }],
+    });
+    const checkProps = getFieldProps('check', {});
+    const formItemLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 12 },
+    };
+    return (
+      <Form horizontal form={this.props.form}>
+        <Modal title="添加新成员" visible={visible}
+               onOk={this.handleOk} onCancel={this.handleCancel}
+               wrapClassName="NewMemberForm"
+               width="463px"
+        >
+          <FormItem
+            {...formItemLayout}
+            label="名称"
+            hasFeedback
+            help={isFieldValidating('name') ? '校验中...' : (getFieldError('name') || []).join(', ')}
+          >
+            <Input {...nameProps} placeholder="新成员名称" />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="类型"
+            hasFeedback
+          >
+            <div>
+              普通成员
+              <Tooltip placement="right" title={text}>
+                <Icon type="question-circle-o" style={{marginLeft: 10}}/>
+              </Tooltip>
+            </div>
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="密码"
+            hasFeedback
+          >
+            <Input {...passwdProps} type="password" autoComplete="off"
+                   onContextMenu={noop} onPaste={noop} onCopy={noop} onCut={noop}
+                   placeholder="新成员名称登录密码"
+            />
+          </FormItem>
+    
+          <FormItem
+            {...formItemLayout}
+            label="确认密码"
+            hasFeedback
+          >
+            <Input {...rePasswdProps} type="password" autoComplete="off" placeholder="请再次输入密码确认"
+                   onContextMenu={noop} onPaste={noop} onCopy={noop} onCut={noop}
+            />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="手机"
+            hasFeedback
+          >
+            <Input {...telProps} type="text" placeholder="新成员手机" />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="邮箱"
+            hasFeedback
+          >
+            <Input {...emailProps} type="email" placeholder="新成员邮箱账号" />
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label=""
+          >
+            <Checkbox className="ant-checkbox-vertical" {...checkProps}>
+              创建完成后, 密码账户名发送至该邮箱
+            </Checkbox>
+          </FormItem>
+        </Modal>
+      </Form>
+    )
+  },
+})
+NewMemberForm = createForm()(NewMemberForm)
 
 export default class MemberManage extends Component {
   constructor(props){
@@ -112,16 +318,29 @@ export default class MemberManage extends Component {
     this.handleSearch = this.handleSearch.bind(this)
     this.handleSelect = this.handleSelect.bind(this)
     this.handleInt = this.handleInt.bind(this)
+    this.showModal = this.showModal.bind(this)
     this.state = {
       selecteData: [],
       selecteValue: '',
       searchValue: '',
       searchResult: [],
       notFound: false,
+      visible: false,
     }
   }
   handleInt(e){
     const { selecteValue } = this.state
+    
+    if(selecteValue === ''){
+      const selecteData = []
+      data.map((item,index) => {
+        selecteData.push(item['name'])
+      })
+      this.setState({
+        selecteValue:'name',
+        selecteData: selecteData
+      })
+    }
     let value = e.target.value
     this.setState({
       searchValue: value
@@ -132,7 +351,6 @@ export default class MemberManage extends Component {
     if(selecteData.length === 0){
       return
     } else {
-      console.log('selecteData',selecteData);
       let result = []
       let searchResult= []
       selecteData.map((item,index) => {
@@ -163,13 +381,17 @@ export default class MemberManage extends Component {
   handleSelect(value){
     const selecteData = []
     data.map((item,index) => {
-      console.log('item',item[`${value}`]);
       selecteData.push(item[`${value}`])
     })
     this.setState({
       selecteValue:value,
       selecteData: selecteData
     })
+  }
+  showModal() {
+    this.setState({
+      visible: true,
+    });
   }
   componentWillMount(){
     this.setState({
@@ -180,6 +402,7 @@ export default class MemberManage extends Component {
   }
   render(){
     const scope = this
+    const { visible } = this.state
     const selectBefore = (
       <Select defaultValue="name" style={{ width: 80 }} onChange={this.handleSelect}>
         <Option value="name">用户名</Option>
@@ -191,10 +414,11 @@ export default class MemberManage extends Component {
     return (
       <div id="MemberManage">
         <Row>
-          <Button type="primary" size="large">
+          <Button type="primary" size="large" onClick={this.showModal}>
             <i className="fa fa-plus"/>
             添加新成员
           </Button>
+            <NewMemberForm visible={visible} scope={scope}/>
           <div className="ant-search-input-wrapper search">
             <Input addonBefore={selectBefore}
                    placeholder="请输入关键词搜索"
