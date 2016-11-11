4be6121b626bd7a7b888d31b872cdb821d124ca5
zhangsh <shouhong@shouhongmatoMacBook-Pro.local>
2016-11-11 19:14:07 +0800

    Get teamspace statistic info

--- controllers/team_manage.js
+++ controllers/team_manage.js
@@ -48,6 +48,14 @@ exports.getTeamspaces = function* () {
   if (result.listMeta && result.listMeta.total) {
     total = result.listMeta.total
   }
+
+  for (let index in teamspaces) {
+    const r = yield api.teams.getBy([teamspaces[index].teamID, "spaces", teamspaces[index].spaceID, "app_info"])
+    teamspaces[index].appCount = r.appCount
+    teamspaces[index].serviceCount = r.serviceCount
+    teamspaces[index].containerCount = r.containerCount
+  } 
+
   this.body = {
     data: teamspaces,
     total,
