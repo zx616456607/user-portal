b86cecd9f4234c6cdce557c88d6ef9a6a6049546
zhaoxy <312364705@qq.com>
2016-11-14 16:03:43 +0800

    状态样式垂直居中

--- src/components/TipSvcDomain/index.js
+++ src/components/TipSvcDomain/index.js
@@ -8,27 +8,97 @@
  * @author ZhaoXueYu
  */
 import React, { Component } from 'react'
-import { Tooltip, } from 'antd'
+import { Tooltip, Badge, Timeline } from 'antd'
 import './style/TipSvcDomain.less'
+
+let Tip = React.createClass({
+  getInitialState(){
+    return {
+      
+    }
+  },
+  render: function(){
+    const { svcDomain } = this.props
+    let item = svcDomain.map((item,index) => {
+      return (
+        <Timeline.Item>{item}</Timeline.Item>
+      )
+    })
+    return (
+      <div id='Tip'>
+        <Timeline>
+          { item }
+        </Timeline>
+      </div>
+    )
+  }
+})
 
 export default class TipSvcDomain extends Component{
   constructor(props){
     super(props)
+    this.popTip = this.popTip.bind(this)
     this.state = {
       
     }
   }
+  popTip(){
+    console.log('POP !');
+  }
   render(){
-    const { svcDomain } = this.props
-    if(svcDomain.length > 0){
+    /*const appDomain = [
+      {
+        name: 'servce1',
+        data: ['10.1.27.1',]
+      },
+      {
+        name: 'servce2',
+        data: ['10.1.27.1', '10.1.27.2', '10.1.27.3', '10.1.27.4', '10.1.27.5',]
+      },
+      {
+        name: 'servce3',
+        data: ['10.1.27.1', '10.1.27.2', '10.1.27.3', '10.1.27.4', '10.1.27.5',]
+      },
+    ]*/
+    const { appDomain } = this.props
+    const svcDomain = ['10.1.27.1', '10.1.27.2', '10.1.27.3', '10.1.27.4', '10.1.27.5',]
+    if(svcDomain){
+      if(svcDomain.length == 0){
+        return (
+          <span>-</span>
+        )
+      } else if (svcDomain.length == 1) {
+        return (
+          <div id='TipSvcDomain'>
+            <a target="_blank" href={svcDomain[0]}>{svcDomain[0]}</a>
+          </div>
+        )
+      } else if (svcDomain.length == 2) {
+        return (
+          <div>
+            <a target="_blank" href={svcDomain[0]}>{svcDomain[0]}</a>
+            <a target="_blank" href={svcDomain[1]}>{svcDomain[1]}</a>
+          </div>
+        )
+      } else if (svcDomain.length > 2) {
+        return (
+          <div>
+            <a target="_blank" href={svcDomain[0]}>{svcDomain[0]}</a>
+            <Tooltip placement="right" title={ <Tip svcDomain={svcDomain}/> }>
+              <Badge count='. . .' onClick={this.popTip}/>
+            </Tooltip>
+          </div>
+        )
+      } else {
+        return (
+          <div></div>
+        )
+      }
+    } else if (appDomain) {
       return (
-        <div id='TipSvcDomain'>
-          <a target="_blank" href={svcDomain[0]}>{svcDomain[0]}</a>
+        <div>
+          
         </div>
-      )
-    } else if (svcDomain.length == 0) {
-      return (
-        <span>-</span>
       )
     }
   }
