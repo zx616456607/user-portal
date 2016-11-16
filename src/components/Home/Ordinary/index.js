ce9c6206c69984d98c98a16ff62a044e4a94fe2c
zhaoxy <312364705@qq.com>
2016-11-16 17:26:28 +0800

    总览/管理员

---
+++ src/components/Home/Ordinary/index.js
@@ -0,0 +1,28 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2016 TenxCloud. All Rights Reserved.
+ *
+ *  Storage list
+ *
+ * v0.1 - 2016/11/16
+ * @author ZhaoXueYu
+ */
+import React, { Component } from 'react'
+import {  } from 'antd'
+import './style/Ordinary.less'
+
+export default class Ordinary extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      
+    }
+  }
+  render(){
+    return (
+      <div id='Ordinary'>
+        Ordinary Ordinary Ordinary
+      </div>
+    )
+  }
+}
