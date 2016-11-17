a1d283fd812c14052d70c93b1f25feb811e92251
yubiao <yubiao@tenxcloud.com>
2016-11-17 17:19:36 +0800

    m配置组中增加配置文件内容显示

--- src/components/AppModule/AppServiceDetail/ComposeGroup.js
+++ src/components/AppModule/AppServiceDetail/ComposeGroup.js
@@ -8,23 +8,23 @@
  * @author GaoJian
  */
 import React, { Component } from 'react'
-import { Card } from 'antd'
+import { Card, Spin, Modal } from 'antd'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 import QueueAnim from 'rc-queue-anim'
 import filter from 'lodash/filter'
-
+import { loadConfigName } from '../../../actions/configs.js'
 import "./style/ComposeGroup.less"
 
 let MyComponent = React.createClass({
-  propTypes : {
-    config : React.PropTypes.array
+  propTypes: {
+    config: React.PropTypes.array
   },
   componentWillMount() {
     const service = this.props.service
     let volumes = service.spec.template.spec.volumes
     const container = service.spec.template.spec.containers[0]
-    if(!volumes) {
+    if (!volumes) {
       this.setState({
         config: []
       })
@@ -48,6 +48,20 @@ let MyComponent = React.createClass({
       config
     })
   },
+	loadConfigData(group, name) {
+    const self = this
+    this.props.loadConfigName(this.props.cluster, { group, Name: name }, {
+      success: {
+        func: (result) => {
+          Modal.confirm({
+            title: '配置文件',
+            content: <pre>{result.data}</pre>,
+            okText: '确定'
+          })
+        }
+      }
+    })
+	},
   componentWillReceiveProps(nextProp) {
     const { serviceDetailmodalShow } = nextProp
     if (!serviceDetailmodalShow) {
@@ -57,7 +71,7 @@ let MyComponent = React.createClass({
       return
     }
     const service = nextProp.service
-    if(!service.spec) {
+    if (!service.spec) {
       this.setState({
         config: []
       })
@@ -88,33 +102,54 @@ let MyComponent = React.createClass({
     this.setState({
       config
     })
+
   },
-  render : function() {
-  let config = this.state.config;
-  let items = config.map((item) => {
+  render: function () {
+    const configData = this.props.configData[this.props.cluster]
+    let loading = ''
+    if(configData) {
+      const { isFetching } = configData
+      
+      if(isFetching) {
+        loading= <div className="loadingBox" style={{position: 'absolute'}}><Spin size="large" /></div>
+      }
+    }
+    let config = this.state.config;
+    let items = config.map((item) => {
+      return (
+        <div className="composeDetail" key={item.id.toString() }>
+          <div className="commonData">
+            <span>{item.mountPod}</span>
+          </div>
+          <div className="commonData">
+            <span>{item.group}</span>
+          </div>
+          <div className="composefile commonData" onClick={() => this.loadConfigData(item.group, item.file) }>
+            <span>{item.file}</span>
+          </div>
+          <div style={{ clear: "both" }}></div>
+        </div>
+      );
+    });
     return (
-      <div className="composeDetail" key={item.id.toString()}>
-        <div className="commonData">
-          <span>{item.mountPod}</span>
-        </div>
-        <div className="commonData">
-          <span>{item.group}</span>
-        </div>
-        <div className="composefile commonData">
-          <span>{item.file}</span>
-        </div>
-        <div style={{clear:"both"}}></div>
-      </div>
-    );
-  });
-  return (
-    <Card className="composeList">
+      <Card className="composeList">
+        {loading}
         { items }
-    </Card>
+      </Card>
     );
   }
 });
 
+function mapStateToProps(state, props) {
+  return {
+	   cluster: state.entities.current.cluster.clusterID,
+				configData: state.configReducers.loadConfigName
+		}
+}
+
+MyComponent = connect(mapStateToProps, {
+  loadConfigName
+})(MyComponent)
 
 export default class ComposeGroup extends Component {
   constructor(props) {
@@ -135,7 +170,7 @@ export default class ComposeGroup extends Component {
           <div className="commonTitle">
             配置文件
           </div>
-          <div style={{clear:"both"}}></div>
+          <div style={{ clear: "both" }}></div>
         </div>
         <MyComponent service={this.props.service} serviceName={this.props.serviceName} cluster={this.props.cluster} serviceDetailmodalShow={this.props.serviceDetailmodalShow}/>
       </div>
@@ -144,5 +179,5 @@ export default class ComposeGroup extends Component {
 }
 
 ComposeGroup.propTypes = {
-//
+  //
 }
