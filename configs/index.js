666474bcb809c92cb3b3f8f6baa9ca3ee887f832
huangqg <huangqg@tenxcloud.com>
2016-12-02 19:39:34 +0800

    fix process issue

--- configs/index.js
+++ configs/index.js
@@ -9,28 +9,34 @@
  */
 'use strict'
 const modes = require('./models');
-const env = process.env
+// Remove the process.dev definition, as configs/index.js will be used in src/routes/index.js which
+// cannot recognition the process from node.js, and it is specific for react
+// const env = process.env
 const config = {
   production: false,
   model: {
     type: 'standard',
     modules: modes['standard']
   },
-  protocol: 'http' || env.DASHBOARD_PROTOCOL,
-  hostname: "0.0.0.0" || env.DASHBOARD_HOST,
-  port: 8003 || env.DASHBOARD_PORT,
-  intl_cookie_name: "intl_locale",
-  session_key: "tce",
-  session_secret: ["tenxcloud_dashboard", "secret_dream008"],
+  // protocol: 'http' || env.DASHBOARD_PROTOCOL,
+  // hostname: "0.0.0.0" || env.DASHBOARD_HOST,
+  protocol: 'http',
+  hostname: '0.0.0.0',
+  port: 8003,
+  intl_cookie_name: 'intl_locale',
+  session_key: 'tce',
+  session_secret: ['tenxcloud_dashboard', 'secret_dream008'],
   session_store: {
-    url: null || env.SESSION_STORE_URL,
-    pass: null || env.SESSION_STORE_PASS
+    // url: null || env.SESSION_STORE_URL,
+    // pass: null || env.SESSION_STORE_PASS
+    url: null,
+    pass: null
   },
   tenx_api: {
-    protocol: "http" || env.TENX_API_PROTOCOL,
+    protocol: 'http',
+    host: '192.168.1.103:48000'
     // host: "localhost:8000" || env.TENX_API_HOST
     // host: "192.168.0.63:8000" || env.TENX_API_HOST
-    host: "192.168.1.103:48000" || env.TENX_API_HOST
     // host: "192.168.0.230:8000" || env.TENX_API_HOST
     // host: "192.168.3.3:48000" || env.TENX_API_HOST
   }
