a1d27c4ce04964559d7f5e441341fbc8a04a341f
zhangpc <zhangpc@tenxcloud.com>
2016-12-12 17:40:10 +0800

    merge code

--- configs/index.js
+++ configs/index.js
@@ -14,7 +14,7 @@ const config = {
   production: false,
   protocol: 'http' || env.DASHBOARD_PROTOCOL,
   hostname: "0.0.0.0" || env.DASHBOARD_HOST,
-  port: 8003,
+  port: 8005,
   intl_cookie_name: 'intl_locale',
   session_key: 'tce',
   session_secret: ['tenxcloud_dashboard', 'secret_dream008'],
@@ -25,7 +25,10 @@ const config = {
   tenx_api: {
     protocol: 'http',
     host: '192.168.1.103:48000' || env.TENX_API_HOST
-    //host: "localhost:8000" || env.TENX_API_HOST
+    // host: "localhost:8000" || env.TENX_API_HOST
+    // host: "192.168.0.63:8000" || env.TENX_API_HOST
+    // host: "192.168.0.230:8000" || env.TENX_API_HOST
+    // host: "192.168.3.3:48000" || env.TENX_API_HOST
   },
   mail_server: {
     host: "smtp.qq.com",
