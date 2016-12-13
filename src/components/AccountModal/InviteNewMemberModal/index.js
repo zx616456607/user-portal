3f033fcdf0357c83cca778f3449e07e1153c0e76
zhaoxy <312364705@qq.com>
2016-12-13 20:24:46 +0800

    team/newMember

--- src/components/AccountModal/InviteNewMemberModal/index.js
+++ src/components/AccountModal/InviteNewMemberModal/index.js
@@ -8,17 +8,31 @@
  * @author ZhaoXueYu
  */
 import React, { Component } from 'react'
-import { Modal,Alert,Icon,Button,Row,Col,Input,Tag } from 'antd'
+import { Modal,Alert,Icon,Button,Row,Col,Input,Tag,Select } from 'antd'
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css'
+
+
+
+const Option = Select.Option;
+let valuesArr = []
+let filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
 
 export default class ExitTeamModal extends Component{
   constructor(props){
     super(props)
     this.handleOk = this.handleOk.bind(this)
     this.handleCancel = this.handleCancel.bind(this)
-    this.handleOnKeyDown = this.handleOnKeyDown.bind(this)
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleTagsChange = this.handleTagsChange.bind(this)
+    this.handleChangeInput = this.handleChangeInput.bind(this)
+    this.renderInput = this.renderInput.bind(this)
+    this.renderTag = this.renderTag.bind(this)
+    this.defaultPasteSplit = this.defaultPasteSplit.bind(this)
     this.state = {
       valueArr: [],
       disabled: false,
+      tags: [],
     }
   }
   handleOk() {
@@ -29,17 +43,14 @@ export default class ExitTeamModal extends Component{
     const { closeInviteModal } = this.props
     closeInviteModal()
   }
-  handleOnKeyDown (e) {
-    console.log('inviteInt',this.refs.inviteInt)
-    let value = e.target.innerHTML
-    console.log('value',value)
-    let valueArr = []
-    var filter  = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
-    console.log('keyCode ',e.keyCode)
-    if (e.keyCode === 186) {
-      valueArr = value.split(';')
-      
-      console.log('valueArr',valueArr)
+  /*handleOnKeyDown (value) {
+    console.log('keyDowm',value)
+    
+    if (value.charAt(value.length - 1) === ';') {
+      console.log('value',value)
+      valuesArr = value.split(';')
+      valuesArr.length -= 1
+      console.log('valuesArr',valuesArr)
       valueArr.map((item,index) => {
         if(filter.test(item)) {
           valueArr.push(
@@ -47,20 +58,109 @@ export default class ExitTeamModal extends Component{
           )
           return
         } else {
-
-          /*valueArr.push(
+          valueArr.push(
             <Tag closable key={index} color='red'>{item}</Tag>
-          )*/
+          )
           this.setState({
             disabled: true
           })
         }
       })
-
+      
       this.setState({
-        valueArr: valueArr
+        valueArr: valuesArr
       })
     }
+  }*/
+  handleOnChange (e) {
+    let valueArr = []
+    console.log('vaule',e.target)
+    console.log('refs',this.refs)
+    console.log('text-----',this.refs.inviteInt.innerHTML)
+    let values = this.refs.inviteInt.innerHTML
+    
+    valueArr = values.split(';')
+    console.log('valueArr',valueArr);
+    valueArr.length -= 1
+    valueArr.map((item,index) => {
+      if(filter.test(item)){
+        console.log('item',item);
+      
+      } else {
+        console.log('err item',item);
+        valuesArr.push(
+          <Tag closable color="red" key={index}>{item}</Tag>
+        )
+        this.setState({
+          valueArr: valuesArr
+        })
+      }
+    })
+  }
+  handleChange (value) {
+    let vaules = []
+    let {valueArr} = this.state
+    console.log('ChangeValue',value)
+    value.map((item,index) => {
+      if (filter.test(item)) {
+        vaules.push(
+          <Tag className='normal'>{value}</Tag>
+        )
+      } else {
+         vaules.push(
+          <Tag className='normal' closable color='red'>{value}</Tag>
+        )
+      }
+    })
+    this.setState({
+      valueArr: vaules
+    })
+  }
+  handleTagsChange (value){
+    this.setState({
+      tags:value
+    })
+  }
+  handleChangeInput(tag) {
+    this.setState({tag})
+  }
+  renderTag (props) {
+    let {tag, key, disabled, onRemove, classNameRemove, getTagDisplayValue, ...other} = props
+    /*if(tag.indexOf(';')>0){
+      console.log('tag',tag.split(';'))
+      
+    }*/
+    if (!filter.test(tag)) {
+      return (
+        <span key={key} {...other} className="react-tagsinput-tag errTags">
+          {getTagDisplayValue(tag)}
+          {!disabled &&
+            <a className={classNameRemove} onClick={(e) => onRemove(key)} >
+            </a>
+          }
+        </span>
+      )
+    } else {
+      return (
+        <span key={key} {...other}>
+          {getTagDisplayValue(tag)}
+          {!disabled &&
+          <a className={classNameRemove} onClick={(e) => onRemove(key)} >
+          </a>
+          }
+        </span>
+      )
+    }
+  }
+  renderInput (props) {
+    let {onChange, value, addTag, ...other} = props
+    return (
+      <input type='text' onChange={onChange} value={value} {...other} />
+    )
+  }
+  defaultPasteSplit (data) {
+    console.log('data',data);
+    return data.split(';').map(d => d.trim())
   }
   render(){
     const { visible } = this.props
@@ -83,26 +183,43 @@ export default class ExitTeamModal extends Component{
                </Button>,
              ]}
       >
-        <Row className='inviteTitle'>
-          <Col span={12}>
-            邮箱邀请
-          </Col>
-          <Col className="inviteCount" span={12}>
-            还可添加20人
-          </Col>
-        </Row>
-        <div
-          placeholder='可输入多个邮箱地址, 邮箱之间用分号" ; "分隔, 每次最多添加20个 .'
-          className='inviteInt ant-select-selection ant-select-selection--multiple'
-          onKeyDown={this.handleOnKeyDown}
-          contentEditable={true}
-          ref='inviteInt'
-        >
-          {
-            valueArr.map((item,index) => {
-              return (item)
-            })
-          }
+        <div className='inviteModal'>
+          <Row className='inviteTitle'>
+            <Col span={12}>
+              邮箱邀请
+            </Col>
+            <Col className="inviteCount" span={12}>
+              还可添加20人
+            </Col>
+          </Row>
+          {/*<Select
+            className='inviteInt'
+            placeholder='可输入多个邮箱地址, 邮箱之间用分号" ; "分隔, 每次最多添加20个 .'
+            onSearch={this.handleOnKeyDown}
+            onChange={this.handleChange}
+            value={valueArr}
+            tags
+            style={{ width: '100%' }}
+            getPopupContainer={() => document.getElementsByClassName('inviteModal')[0]}
+          >
+          </Select>
+          <div
+            contentEditable={true}
+            className='inviteInt'
+            onKeyUp={this.handleOnChange}
+            ref='inviteInt'
+          >
+            {
+              valueArr
+            }
+          </div>*/}
+          <TagsInput value={this.state.tags}
+                     onChange={this.handleTagsChange}
+                     renderTag={this.renderTag}
+                     renderInput={this.renderInput}
+                     inputProps={{className: 'react-tagsinput-input', placeholder: ''}}
+                     pasteSplit={this.defaultPasteSplit}
+          />
         </div>
       </Modal>
     )
