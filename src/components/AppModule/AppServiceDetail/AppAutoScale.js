b24f27eb8c9fe6135968030a53adfc6a4c5d6d99
Xin Huang <649527642@qq.com>
2016-12-27 19:49:43 +0800

    refs CRYSTAL-2010. Disabled auto scaling if a volume is mounted by the service

--- src/components/AppModule/AppServiceDetail/AppAutoScale.js
+++ src/components/AppModule/AppServiceDetail/AppAutoScale.js
@@ -42,7 +42,8 @@ class AppAutoScale extends Component {
       maxReplicas: props.autoScale.maxReplicas || 2,
       targetCPUUtilizationPercentage: props.autoScale.targetCPUUtilizationPercentage || 30,
       saveText: '保存',
-      isAutoScaleOpen: props.isAutoScaleOpen
+      isAutoScaleOpen: props.isAutoScaleOpen,
+      isAvailable: false
     }
   }
 
@@ -51,17 +52,28 @@ class AppAutoScale extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { cluster, serviceName, autoScale, replicas, isAutoScaleOpen } = nextProps
+    const { cluster, serviceName, autoScale, replicas, isAutoScaleOpen, volumes } = nextProps
     this.setState({
       isAutoScaleOpen: isAutoScaleOpen
     })
     if (serviceName === this.props.serviceName) {
       return
     }
+    let available = true
+    if (volumes) {
+      for (var i in volumes) {
+        if (volumes[i].rbd) {
+          available = false
+          break
+        }
+      }
+    }
+
     this.setState({
       minReplicas: autoScale.minReplicas || replicas,
       maxReplicas: autoScale.maxReplicas || replicas,
-      targetCPUUtilizationPercentage: autoScale.targetCPUUtilizationPercentage || 30
+      targetCPUUtilizationPercentage: autoScale.targetCPUUtilizationPercentage || 30,
+      isAvailable: available
     })
     loadData(nextProps)
   }
@@ -218,8 +230,9 @@ class AppAutoScale extends Component {
                 <div>
                   <Tooltip
                     arrowPointAtCenter
-                    title={isAutoScaleOpen ? '弹性伸缩已开启' : '弹性伸缩已关闭'} >
+                    title={this.state.isAvailable ? (isAutoScaleOpen ? '弹性伸缩已开启' : '弹性伸缩已关闭') : '不允许弹性伸缩'} >
                     <Switch
+                      disabled={!this.state.isAvailable}
                       onChange={this.handleSwitch}
                       checkedChildren="开" unCheckedChildren="关"
                       checked={isAutoScaleOpen}
@@ -242,7 +255,9 @@ class AppAutoScale extends Component {
             }
           </div>
         </div>
-        <Alert message="注: 系统将根据设定的CPU阈值来自动的『扩展,或减少』该服务所『缺少,或冗余』的实例数量" type="info" />
+        {this.state.isAvailable ? 
+          <Alert message="注: 系统将根据设定的CPU阈值来自动的『扩展,或减少』该服务所『缺少,或冗余』的实例数量" type="info" /> :
+          <Alert message="注: 已挂载存储卷的服务不允许设置弹性伸缩" type="info" />}
         <Card>
           <Row className="cardItem">
             <Col className="itemTitle" span={4} style={{ textAlign: 'right' }}>服务名称</Col>
