7804c18297293506a70563b5b6a5a6820ecaded5
gaojian <gaojian@tenxcloud.com>
2016-12-27 18:25:24 +0800

    fix bug

--- src/components/AccountModal/_Enterprise/TeamManage/index.js
+++ src/components/AccountModal/_Enterprise/TeamManage/index.js
@@ -217,13 +217,13 @@ let TeamTable = React.createClass({
       onShowSizeChange(current, pageSize) {
         scope.props.loadUserTeamList('default', {
           page: current,
-          size: pageSize,
+          size: 10,
           sort,
           filter,
         })
         scope.setState({
           page: current,
-          pageSize: pageSize,
+          pageSize: 10,
           current: 1,
         })
       },
