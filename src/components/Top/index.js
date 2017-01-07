dd64e359c52e094429adc04485594b015e6356ad
huangqg <huangqg@tenxcloud.com>
2017-01-07 22:56:42 +0800

    adjust login page for enterprise

--- src/components/Top/index.js
+++ src/components/Top/index.js
@@ -11,6 +11,8 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import './style/Top.less'
 import { Link } from 'react-router'
+const standard = require('../../../configs/constants').STANDARD_MODE
+const mode = require('../../../configs/model').mode
 
 export default class Top extends Component {
   constructor (props) {
@@ -20,39 +22,63 @@ export default class Top extends Component {
   	}
   }
   render(){
-  	return (
-  	  <div id='Top'>
-  	  	<div className='topWrap'>
-  	  	  <div className='topLogo'>
-            <a href='/'>
-              <img src="/img/TopLogo.svg" alt="logo" className="logo" />
-            </a>
+    if (mode === standard) {
+      return (
+        <div id='Top'>
+          <div className='topWrap'>
+            <div className='topLogo'>
+              <a href='/'>
+                <img src="/img/TopLogo.svg" alt="logo" className="logo" />
+              </a>
+            </div>
+            <div className='topNav'>
+              <div className='navItem'>
+                <a href='https://www.tenxcloud.com' target='_blank'>官网首页</a>
+              </div>
+              <div className='navItem'>
+                <a href='http://docs.tenxcloud.com' target='_blank'>文档中心</a>
+              </div>
+              <div className='navItem'>
+                <a href='http://open.tenxcloud.com' target='_blank'>问答社区</a>
+              </div>
+              <div className='navItem'>
+                <a href='http://blog.tenxcloud.com' target='_blank'>技术博客</a>
+              </div>
+              <span style={{lineHeight:'25px'}}>|</span>
+              <div className='log'>
+                <div className='navItem' style={{marginLeft: 0}}>
+                  <Link to='/login'>登录</Link>
+                </div>
+                <div className='navRegister'>
+                  <Link to='/register'>注册</Link>
+                </div>
+              </div>
+            </div>
           </div>
-  	  	  <div className='topNav'>
-  	  	    <div className='navItem'>
-              <a href='https://www.tenxcloud.com' target='_blank'>官网首页</a>
+        </div>
+      )
+    } else {
+      return (
+        <div id='Top'>
+          <div className='topWrap'>
+            <div className='topLogo'>
+              <a href='/'>
+                <img src="/img/TopLogo.svg" alt="logo" className="logo" />
+              </a>
             </div>
-  	  	    <div className='navItem'>
-              <a href='http://docs.tenxcloud.com' target='_blank'>文档中心</a>
-            </div>
-  	  	    <div className='navItem'>
-              <a href='http://open.tenxcloud.com' target='_blank'>问答社区</a>
-            </div>
-            <div className='navItem'>
-              <a href='http://blog.tenxcloud.com' target='_blank'>技术博客</a>
-            </div>
-  	  	    <span style={{lineHeight:'25px'}}>|</span>
-  	  	    <div className='log'>
-  	  	      <div className='navItem' style={{marginLeft: 0}}>
-                <Link to='/login'>登录</Link>
+            <div className='topNav'>
+              <div className='log'>
+                <div className='navItem' style={{marginLeft: 0}}>
+                  <a href='https://www.tenxcloud.com' target='_blank'>官网首页</a>
+                </div>
+                <div className='navItem' style={{marginLeft: 0}}>
+                  <a href='http://docs.tenxcloud.com' target='_blank'>文档中心</a>
+                </div>
               </div>
-  	  	      <div className='navRegister'>
-                <Link to='/register'>注册</Link>
-              </div>
-  	        </div>
-  	  	  </div>
-  	  	</div>
-  	  </div>
-	)
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
 }
