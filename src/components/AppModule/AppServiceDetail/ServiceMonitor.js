de7ffc0b6d9b90574b7222a8cc442cf0d2e2786b
wanglei <wanglei@tenxcloud.com>
2017-01-11 21:59:10 +0800

    Update ServiceMonitor.js

--- src/components/AppModule/AppServiceDetail/ServiceMonitor.js
+++ src/components/AppModule/AppServiceDetail/ServiceMonitor.js
@@ -84,7 +84,7 @@ class ServiceMonitior extends Component {
       })
       metricsInterval = setInterval(() => {    
         loadServiceAllOfMetrics(cluster, serviceName, query)
-      }, 5000);
+      }, 60000);
     }
   }
 
