3626da99c07b0fa128c6d7befbcb1deea7e8dc45
zhangpc <zhangpc@tenxcloud.com>
2017-01-11 19:59:11 +0800

    change IE9- tips

--- static/js/lib/ie-blocker/ie-blocker.en.js
+++ static/js/lib/ie-blocker/ie-blocker.en.js
@@ -1,65 +1,65 @@
 (function () {
     var ibContainer;
+    var html;
     var browserIcons;
     var scriptPath;
-    var browserName;
     var imgPath;
-    var html;
+    var browserName;
 
     ibContainer = document.createElement('div');
     ibContainer.setAttribute('id', 'ib-container');
 
-    html = "";
     html += "<div class=\"ib-modal\">";
-    html += "    <div class=\"ib-header\">";
-    html += "        <h1>You are using an outdated browser.<\/h1>";
-    html += "        <p>To better experience our services and protect your information,";
-    html += "            <br><strong>please use one of these up-to-date, free and excellent browsers.<\/strong>";
-    html += "        <\/p>";
+    html += "    <div class=\"ib-up\">";
+    html += "        <div class=\"ib-header\">";
+    html += "            <p class=\"ib-h1\">Browser version is too low, please upgrade immediately !<\/p>";
+    html += "            <p>Visit the ";
+    html += "                <a href=\"https:\/\/portal.tenxcloud.com\">";
+    html += "                    <strong>Portal | Tenxcloud<\/strong> ";
+    html += "                </a>";
+    html += "                for a better speed experience using the following browsers.";
+    html += "            <\/p>";
+    html += "        <\/div>";
     html += "    <\/div>";
-    html += "    <ul class=\"ib-browsers\">";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.google.com\/chrome\/browser\/desktop\/index.html\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-chrome\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Chrome<\/div>";
-    html += "                <div class=\"ib-browser-description\">A fast new browser from Google.<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.firefox.com\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-firefox\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Firefox<\/div>";
-    html += "                <div class=\"ib-browser-description\">Your online security is Firefox's top priority<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"https:\/\/www.apple.com\/safari\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-safari\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Safari<\/div>";
-    html += "                <div class=\"ib-browser-description\">Safari for Mac from Apple, the world's most innovative browser.<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.opera.com\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-opera\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Opera<\/div>";
-    html += "                <div class=\"ib-browser-description\">The fastest browser on Earth.<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/windows.microsoft.com\/en-US\/internet-explorer\/download-ie\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-ie11\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">IE 11<\/div>";
-    html += "                <div class=\"ib-browser-description\">Designed to help you take control of your privacy. Free from Microsoft.<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "    <\/ul>";
-    html += "    <div class=\"ib-footer\">";
-    html += "        <a class=\"ib-try\" href=\"http:\/\/www.google.com\/chrome\/browser\/desktop\/index.html\">Try Chrome now!<\/a>";
+    html += "    <div class=\"ib-down\">";
+    html += "        <ul class=\"ib-browsers\">";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.google.cn\/chrome\/browser\/desktop\/index.html\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-chrome\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Chrome<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.firefox.com.cn\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-firefox\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Firefox<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"https:\/\/www.apple.com\/cn\/safari\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-safari\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Safari<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.opera.com\/zh-cn\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-opera\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Opera<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/windows.microsoft.com\/zh-cn\/internet-explorer\/download-ie\">";
+    html += "                        <i class=\"ib-browser-icon ib-ua-ie11\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">IE 11<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "        <\/ul>";
+    html += "        <div class=\"ib-footer\">";
+    html += "            <a class=\"ib-try\" href=\"http:\/\/www.google.com\/chrome\/browser\/desktop\/index.html\">Try Chrome<\/a>";
+    html += "        <\/div>";
     html += "    <\/div>";
     html += "<\/div>";
     html += "<div class=\"ib-mask\"><\/div>";
-    html += "";
 
     ibContainer.innerHTML = html;
 
@@ -67,7 +67,6 @@
     browserIcons = ibContainer.getElementsByTagName('i');
 
     for (var i = 0; i < document.scripts.length; i++) {
-
         if (match = document.scripts[i].src.match(/(.*)ie-blocker.en\.js/)) {
             scriptPath = match[1];
             imgPath = scriptPath + (document.scripts[i].getAttribute('img-path') || 'img/');
@@ -78,14 +77,12 @@
     for (var i = 0; i < browserIcons.length; i++) {
         if (browserName = browserIcons[i].className.match(/ib-ua-(\w+)/)[1]) {
             browserIcons[i].style['filter'] = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' +
-                imgPath + browserName + '.png")';
+                imgPath + browserName + '.png",sizingMethod="scale"))';
         }
     }
 
     window.onload = function () {
-
         document.body.appendChild(ibContainer);
-
         ibContainer.style.display = 'block';
     };
 })();
--- static/js/lib/ie-blocker/ie-blocker.zh.js
+++ static/js/lib/ie-blocker/ie-blocker.zh.js
@@ -1,66 +1,65 @@
 (function () {
     var ibContainer;
+    var html;
     var browserIcons;
     var scriptPath;
-    var browserName;
     var imgPath;
-    var html;
+    var browserName;
 
     ibContainer = document.createElement('div');
     ibContainer.setAttribute('id', 'ib-container');
 
-    var html="";
     html += "<div class=\"ib-modal\">";
-    html += "    <div class=\"ib-header\">";
-    html += "        <h1>您的浏览器需要更新<\/h1>";
-    html += "        <p>为了更好的体验我们的服务并保护您的个人信息，";
-    html += "            <br><strong>请使用以下新版浏览器<\/strong>";
-    html += "        <\/p>";
+    html += "    <div class=\"ib-up\">";
+    html += "        <div class=\"ib-header\">";
+    html += "            <p class=\"ib-h1\">检测到该浏览器版本过低，建议马上升级！<\/p>";
+    html += "            <p>为了访问 ";
+    html += "                <a href=\"https:\/\/portal.tenxcloud.com\">";
+    html += "                    <strong>控制台Portal | 时速云<\/strong> ";
+    html += "                </a>";
+    html += "                具有更佳的极速体验，请使用以下浏览器进行访问。";
+    html += "            <\/p>";
+    html += "        <\/div>";
     html += "    <\/div>";
-    html += "    <ul class=\"ib-browsers\">";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.google.cn\/chrome\/browser\/desktop\/index.html\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-chrome\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Chrome<\/div>";
-    html += "                <div class=\"ib-browser-description\">谷歌全新推出的高速浏览器<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.firefox.com.cn\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-firefox\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Firefox<\/div>";
-    html += "                <div class=\"ib-browser-description\">Firefox注重您的信息安全<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"https:\/\/www.apple.com\/cn\/safari\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-safari\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Safari<\/div>";
-    html += "                <div class=\"ib-browser-description\">Safari是苹果公司充满创新设计的产品，可用于Mac<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/www.opera.com\/zh-cn\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-opera\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">Opera<\/div>";
-    html += "                <div class=\"ib-browser-description\">地球上最快的浏览器<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "        <li>";
-    html += "            <a href=\"http:\/\/windows.microsoft.com\/zh-cn\/internet-explorer\/download-ie\">";
-    html += "                <i class=\"ib-browser-icon ib-ua-ie11\"><\/i>";
-    html += "                <div class=\"ib-browser-name\">IE 11<\/div>";
-    html += "                <div class=\"ib-browser-description\">来自微软的免费浏览器，全新特性助您保护个人隐私<\/div>";
-    html += "            <\/a>";
-    html += "        <\/li>";
-    html += "    <\/ul>";
-    html += "    <div class=\"ib-footer\">";
-    html += "        <a class=\"ib-try\" href=\"http:\/\/www.google.com\/chrome\/browser\/desktop\/index.html\">试试Chrome<\/a>";
+    html += "    <div class=\"ib-down\">";
+    html += "        <ul class=\"ib-browsers\">";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.google.cn\/chrome\/browser\/desktop\/index.html\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-chrome\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Chrome<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.firefox.com.cn\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-firefox\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Firefox<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"https:\/\/www.apple.com\/cn\/safari\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-safari\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Safari<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/www.opera.com\/zh-cn\">";
+    html += "                    <i class=\"ib-browser-icon ib-ua-opera\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">Opera<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "            <li>";
+    html += "                <a href=\"http:\/\/windows.microsoft.com\/zh-cn\/internet-explorer\/download-ie\">";
+    html += "                        <i class=\"ib-browser-icon ib-ua-ie11\"><\/i>";
+    html += "                    <div class=\"ib-browser-name\">IE 11<\/div>";
+    html += "                <\/a>";
+    html += "            <\/li>";
+    html += "        <\/ul>";
+    html += "        <div class=\"ib-footer\">";
+    html += "            <a class=\"ib-try\" href=\"http:\/\/www.google.com\/chrome\/browser\/desktop\/index.html\">试试Chrome<\/a>";
+    html += "        <\/div>";
     html += "    <\/div>";
     html += "<\/div>";
     html += "<div class=\"ib-mask\"><\/div>";
-    html += "";
-
 
     ibContainer.innerHTML = html;
 
@@ -68,7 +67,6 @@
     browserIcons = ibContainer.getElementsByTagName('i');
 
     for (var i = 0; i < document.scripts.length; i++) {
-
         if (match = document.scripts[i].src.match(/(.*)ie-blocker.zh\.js/)) {
             scriptPath = match[1];
             imgPath = scriptPath + (document.scripts[i].getAttribute('img-path') || 'img/');
@@ -79,14 +77,12 @@
     for (var i = 0; i < browserIcons.length; i++) {
         if (browserName = browserIcons[i].className.match(/ib-ua-(\w+)/)[1]) {
             browserIcons[i].style['filter'] = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' +
-                imgPath + browserName + '.png")';
+                imgPath + browserName + '.png",sizingMethod="scale"))';
         }
     }
 
     window.onload = function () {
-
         document.body.appendChild(ibContainer);
-
         ibContainer.style.display = 'block';
     };
 })();
