cd75c0d38bfaacf5c68a3b03d06f34e265cbf3e2
gaojian <gaojian@tenxcloud.com>
2017-01-19 18:09:06 +0800

    fix bug

--- static/js/svg.js
+++ static/js/svg.js
@@ -1,24 +1,24 @@
-!function(){
+! function() {
   //  改了url 地址 下面removeItem() 的地址也相改一下（remove 上一次url的名称）
-    var url = "/img/sider/svg/svg-symbols.svg?ver=14";
-    var div = document.createElement("div");
-    div.style.display = "none";
-    document.body.appendChild(div);
-    // 载入SVG
-    if (localStorage.getItem(url)) {
-      // 本地获取，减少请求
-      div.innerHTML = localStorage.getItem(url);
-    } else {
-      localStorage.removeItem("/img/sider/svg/svg-symbols.svg?ver=15");
-      var xhr = new XMLHttpRequest();
-      xhr.open("get", url);
-      xhr.onload = function() {
-        if (xhr.responseText) {
-          div.innerHTML = xhr.responseText;
-          // 本地存储
-          localStorage.setItem(url, xhr.responseText);
-        }
-      };
-      xhr.send(null);
-    }
-  }();
+  var url = "/img/sider/svg/svg-symbols.svg?ver=14";
+  var div = document.createElement("div");
+  div.style.display = "none";
+  document.body.appendChild(div);
+  // 载入SVG
+  if(localStorage.getItem(url)) {
+    // 本地获取，减少请求
+    div.innerHTML = localStorage.getItem(url);
+  } else {
+    localStorage.removeItem("/img/sider/svg/svg-symbols.svg?ver=15");
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url);
+    xhr.onload = function() {
+      if(xhr.responseText) {
+        div.innerHTML = xhr.responseText;
+        // 本地存储
+        localStorage.setItem(url, xhr.responseText);
+      }
+    };
+    xhr.send(null);
+  }
+}();
