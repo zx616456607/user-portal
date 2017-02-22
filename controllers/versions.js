dfe05ebb1dec209a75cbce81cefba4c8260cca85
zhangpc <zhangpc@tenxcloud.com>
2017-02-22 19:44:42 +0800

    add miss file for check version

---
+++ controllers/versions.js
@@ -0,0 +1,42 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2017 TenxCloud. All Rights Reserved.
+ *
+ */
+/**
+ * Version controller
+ *
+ * v0.1 - 2017-02-22
+ * @author Zhangpc
+ */
+'use strict'
+
+const apiFactory = require('../services/api_factory')
+
+exports.checkVersion = function* () {
+  const loginUser = this.session.loginUser
+  const query = this.query || {}
+  const specifyConfig = {
+    protocol: 'https',
+    host: 'apiv2.tenxcloud.com',
+  }
+  const spi = apiFactory.getSpi(loginUser, specifyConfig)
+  try {
+    const result = yield spi.versions.get(query)
+    this.body = {
+      query,
+      data: result.data
+    }
+  } catch (error) {
+    let statusCode = error.statusCode
+    if (statusCode >= 500) {
+      this.status = 504
+      this.body = {
+        statusCode: 504,
+        message: 'Gateway Timeout'
+      }
+      return
+    }
+    throw error
+  }
+}
