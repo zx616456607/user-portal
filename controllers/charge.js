e03accf217fac6f7f4d617efb6e8076ab86084e6
zhangsh <shouhong@shouhongmatoMacBook-Pro.local>
2017-02-27 10:13:36 +0800

    add charge apis

---
+++ controllers/charge.js
@@ -0,0 +1,38 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2017 TenxCloud. All Rights Reserved.
+ *
+ * Charge controller
+ *
+ * v0.1 - 2017-02-27
+ * @author shouhong.zhang
+ */
+'use strict'
+
+const apiFactory = require('../services/api_factory')
+
+exports.chargeUser = function* () {
+  const loginUser = this.session.loginUser
+  const spi = apiFactory.getSpi(loginUser)
+  const chargeInfo = this.request.body
+  if (!chargeInfo || !chargeInfo.ids || !chargeInfo.amount) {
+    const err = new Error('ids and amount are required.')
+    err.status = 400
+    throw err
+  }
+  const result = yield spi.payments.createBy(['user'], null, chargeInfo)
+  this.body = result
+}
+
+exports.chargeTeamspace = function* () {
+  const loginUser = this.session.loginUser
+  const spi = apiFactory.getSpi(loginUser)
+  const chargeInfo = this.request.body
+  if (!chargeInfo || !chargeInfo.ids || !chargeInfo.amount) {
+    const err = new Error('ids and amount are required.')
+    err.status = 400
+    throw err
+  }
+  const result = yield spi.payments.createBy(['teamspace'], null, chargeInfo)
+  this.body = result
+}
