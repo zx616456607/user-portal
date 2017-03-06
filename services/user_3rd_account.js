58f39feb1496bd72bbd0ca67d4b6e0eb40c90a65
zhangpc <zhangpc@tenxcloud.com>
2017-03-06 10:35:18 +0800

    edit template content

--- services/user_3rd_account.js
+++ services/user_3rd_account.js
@@ -12,11 +12,13 @@ const moment = require('moment')
 const apiFactory = require('./api_factory')
 const wechatApi = require('../3rd_account/wechat')
 const constants = require('../constants')
+const logger = require('../utils/logger').getLogger('services/user_3rd_account')
 const wechat = new wechatApi()
 
 exports.sendTemplateToWechatLoginUser = function (user) {
-  const TEMPLATE_ID = 'ZbvwDgYXkdImQlThudqfQxP8bvf5pfdqHOBlEgZ5Q3s'
-  const color = '#173177'
+  const method = 'sendTemplateToWechatLoginUser'
+  const TEMPLATE_ID = 'IgjIeRv2j-ApC5CN9dPGxoh5toMToPhHhXkwvQeqesg'
+  const color = '#586C95'
   const api = apiFactory.getApi(user)
   const userID = user.id
   const reqArray = []
@@ -38,32 +40,27 @@ exports.sendTemplateToWechatLoginUser = function (user) {
         return true
       })
     } catch (error) {
-      //
+      // Catch error here
+      logger.error(method, error.stack)
     }
     if (!openid) {
-      console.log(`openid not found.`)
+      logger.warn(method, `get wechat user openid failed`)
       return
     }
     const balance = (user.balance / constants.AMOUNT_CONVERSION) * 100 / 100
     const data = {
       first: {
-        value: '登录成功提醒（通过微信扫描登录操作）',
-        color
+        value: `您好，您已在「时速云 | 公有云」成功登录\n`,
       },
       keyword1: {
         value: user.user,
         color
       },
       keyword2: {
-        value: '时速云 | 公有云',
-        color
-      },
-      keyword3: {
-        value: moment().format('YYYY-MM-DD hh:mm'),
-        color
+        value: moment().format('YYYY-MM-DD hh:mm:ss'),
       },
       remark: {
-        value: `帐号余额：${balance}元\n【客服电话：400-626-1876 | 控制台右下角工单】`
+        value: `账号余额：${balance}元\n\n[客服电话：400-626-1876 | 控制台右下角工单]`
       },
     }
     wechat.sendTemplate(access_token, openid, TEMPLATE_ID, data)
