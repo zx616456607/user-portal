1dcbddb08b1e7d0e9b68d5f983cb3a51d00e9f77
zhangchengzheng <zhangchengzheng@tenxcloud.com>
2017-03-09 17:04:04 +0800

    format

--- src/components/SettingModal/GlobalConfig/index.js
+++ src/components/SettingModal/GlobalConfig/index.js
@@ -7,38 +7,38 @@
 	* v0.1 - 2017/3/7
 	* @author ZhangChengZheng
 	*/
-import React, {Component} from 'react'
-import {Row, Col, Icon, Form, Button, Input} from 'antd'
+import React, { Component } from 'react'
+import { Row, Col, Icon, Form, Button, Input } from 'antd'
 import './style/GlobalConfig.less'
-import  EmailImg from '../../../assets/img/setting/globalconfigEmail.png'
-import  conInter from '../../../assets/img/setting/globalconfigCICD.png'
-import  MirrorImg from '../../../assets/img/setting/globalconfigmirror.png'
-import  CephImg from '../../../assets/img/setting/globalconfigceph.png'
-import {connect} from 'react-redux'
+import EmailImg from '../../../assets/img/setting/globalconfigEmail.png'
+import conInter from '../../../assets/img/setting/globalconfigCICD.png'
+import MirrorImg from '../../../assets/img/setting/globalconfigmirror.png'
+import CephImg from '../../../assets/img/setting/globalconfigceph.png'
+import { connect } from 'react-redux'
 
 const FormItem = Form.Item
 
 //邮件报警
 let Emaill = React.createClass({
-	getInitialState(){
+	getInitialState() {
 		return {
 			isEve: false
 		}
 	},
-	handleReset(e){
+	handleReset(e) {
 		e.preventDefault();
 		console.log("取消")
 		this.props.form.resetFields();
 		this.props.emailChange();
 	},
 	handEve() {
-		this.setState({isEve: !this.state.isEve})
+		this.setState({ isEve: !this.state.isEve })
 	},
 	handleEmail() {
 		this.props.emailChange()
 	},
 	checkService(rule, value, callback) {
-		const {validateFields} = this.props.form
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写服务器地址')])
 			return
@@ -50,7 +50,7 @@ let Emaill = React.createClass({
 		callback()
 	},
 	checkPass(rule, value, callback) {
-		const {validateFields} = this.props.form
+	  const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -66,12 +66,12 @@ let Emaill = React.createClass({
 		callback()
 	},
 	render() {
-		const {emailDisable, emailChange} = this.props
-		const {getFieldProps, getFieldError, isFieldValidating} = this.props.form
+		const { emailDisable, emailChange } = this.props
+		const { getFieldProps, getFieldError, isFieldValidating } = this.props.form
 		//邮件服务器
 		const serviceProps = getFieldProps('service', {
 			rules: [
-				{validator: this.checkService}
+				{ validator: this.checkService }
 			]
 		});
 
@@ -79,12 +79,12 @@ let Emaill = React.createClass({
 		const emailProps = getFieldProps('email', {
 			validate: [{
 				rules: [
-					{required: true, message: '请输入邮箱地址'},
+					{ required: true, message: '请输入邮箱地址' },
 				],
 				trigger: 'onBlur',
 			}, {
 				rules: [
-					{type: 'email', message: '请输入正确的邮箱地址'},
+					{ type: 'email', message: '请输入正确的邮箱地址' },
 				],
 				trigger: ['onBlur', 'onChange'],
 			}],
@@ -93,52 +93,52 @@ let Emaill = React.createClass({
 		//密码
 		const passwordProps = getFieldProps('password', {
 			rules: [
-				{validator: this.checkPass}
+				{ validator: this.checkPass }
 			]
 		});
 		return (
-				<div className="GlobalConfigEmail">
-					<div className="title">邮件报警</div>
-					<div className="content">
-						<div className="contentMain">
-							<div className="contentImg">
-								<img src={ EmailImg } alt="邮件报警"/>
-							</div>
-							<div className="contentkeys">
-								<div className="key">邮件服务器</div>
-								<div className="key">邮箱</div>
-								<div className="key">密码</div>
-							</div>
-							<div className="contentForm">
-								<Form horizontal className="contentFormMain">
-									<FormItem hasFeedback>
-										<Input {...serviceProps} placeholder="如：smtp.exmail.qq.com:25" disabled={emailDisable}/>
-									</FormItem>
-									<FormItem hasFeedback>
-										<Input className="temInput1" {...emailProps} type="email" placeholder="邮箱地址" disabled={emailDisable}/>
-									</FormItem>
-									<FormItem hasFeedback>
-										<i className={this.state.isEve ? 'fa fa-eye activeEve' : 'fa fa-eye-slash activeEve'}
-										   onClick={() => this.handEve()}></i>
-										<Input {...passwordProps} type={ this.state.isEve ? "text" : "password"} placeholder="请输入密码"
-										       disabled={emailDisable}/>
-									</FormItem>
-									<FormItem>
-										{
-											emailDisable
-													? <Button type='primary' className="itemInputLeft" onClick={this.handleEmail}>编辑</Button>
-													: ([
+			<div className="GlobalConfigEmail">
+				<div className="title">邮件报警</div>
+				<div className="content">
+					<div className="contentMain">
+						<div className="contentImg">
+							<img src={EmailImg} alt="邮件报警" />
+						</div>
+						<div className="contentkeys">
+							<div className="key">邮件服务器</div>
+							<div className="key">邮箱</div>
+							<div className="key">密码</div>
+						</div>
+						<div className="contentForm">
+							<Form horizontal className="contentFormMain">
+								<FormItem hasFeedback>
+									<Input {...serviceProps} placeholder="如：smtp.exmail.qq.com:25" disabled={emailDisable} />
+								</FormItem>
+								<FormItem hasFeedback>
+									<Input className="temInput1" {...emailProps} type="email" placeholder="邮箱地址" disabled={emailDisable} />
+								</FormItem>
+								<FormItem hasFeedback>
+									<i className={this.state.isEve ? 'fa fa-eye activeEve' : 'fa fa-eye-slash activeEve'}
+										onClick={() => this.handEve()}></i>
+									<Input {...passwordProps} type={this.state.isEve ? "text" : "password"} placeholder="请输入密码"
+										disabled={emailDisable} />
+								</FormItem>
+								<FormItem>
+									{
+										emailDisable
+											? <Button type='primary' className="itemInputLeft" onClick={this.handleEmail}>编辑</Button>
+											: ([
 												<Button type='primary' className="itemInputLeft" onClick={this.handleEmail}>保存</Button>,
 												<Button onClick={this.handleReset} disabled={emailDisable}>取消</Button>
 											])
-										}
+									}
 
-									</FormItem>
-								</Form>
-							</div>
+								</FormItem>
+							</Form>
 						</div>
 					</div>
 				</div>
+			</div>
 		)
 	}
 })
@@ -151,8 +151,8 @@ let ConInter = React.createClass({
 	handleReset() {
 		this.props.cicdeditChange()
 	},
-	checkCicd(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkCicd(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -168,44 +168,44 @@ let ConInter = React.createClass({
 		callback()
 	},
 	render() {
-		const {cicdeditDisable, cicdeditChange} = this.props
-		const {getFieldProps, getFieldError, isFieldValidating} = this.props.form
+		const { cicdeditDisable, cicdeditChange } = this.props
+		const { getFieldProps, getFieldError, isFieldValidating } = this.props.form
 		const cicdProps = getFieldProps('cicd', {
 			rules: [
-				{validator: this.checkCicd}
+				{ validator: this.checkCicd }
 			]
 		});
 		return (
-				<div className="conInter">
-					<div className="title">持续集成</div>
-					<div className="content">
-						<div className="contentMain">
-							<div className="contentImg">
-								<img src={conInter} alt="持续集成"/>
-							</div>
-							<div className="contentkeys">
-								<div className="key">主机地址</div>
-							</div>
-							<div className="contentForm">
-								<Form horizontal className="contentFormMain">
-									<FormItem hasFeedback>
-										<Input {...cicdProps} placeholder="http://192.168.1.103:38090" disabled={ cicdeditDisable }/>
-									</FormItem>
-									<FormItem>
-										{
-											cicdeditDisable
-													? <Button type='primary' className="itemInputLeft" onClick={this.handleCicd}>编辑</Button>
-													: ([
+			<div className="conInter">
+				<div className="title">持续集成</div>
+				<div className="content">
+					<div className="contentMain">
+						<div className="contentImg">
+							<img src={conInter} alt="持续集成" />
+						</div>
+						<div className="contentkeys">
+							<div className="key">主机地址</div>
+						</div>
+						<div className="contentForm">
+							<Form horizontal className="contentFormMain">
+								<FormItem hasFeedback>
+									<Input {...cicdProps} placeholder="http://192.168.1.103:38090" disabled={cicdeditDisable} />
+								</FormItem>
+								<FormItem>
+									{
+										cicdeditDisable
+											? <Button type='primary' className="itemInputLeft" onClick={this.handleCicd}>编辑</Button>
+											: ([
 												<Button type='primary' className="itemInputLeft" onClick={this.handleCicd}>保存</Button>,
 												<Button onClick={this.handleReset}>取消</Button>
 											])
-										}
-									</FormItem>
-								</Form>
-							</div>
+									}
+								</FormItem>
+							</Form>
 						</div>
 					</div>
 				</div>
+			</div>
 		)
 	}
 })
@@ -219,8 +219,8 @@ let MirrorService = React.createClass({
 		this.props.mirrorChange()
 	},
 	// 镜像服务地址校验规则
-	checkMirror(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkMirror(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -237,8 +237,8 @@ let MirrorService = React.createClass({
 	},
 
 	// 认证服务地址校验规则
-	checkApprove(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkApprove(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -255,8 +255,8 @@ let MirrorService = React.createClass({
 	},
 
 	// 扩展服务地址校验规则
-	checkExtend(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkExtend(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -272,65 +272,65 @@ let MirrorService = React.createClass({
 		callback()
 	},
 	render() {
-		const {mirrorDisable, mirrorChange} = this.props
-		const {getFieldProps, getFieldError, isFieldValidating} = this.props.form
+		const { mirrorDisable, mirrorChange } = this.props
+		const { getFieldProps, getFieldError, isFieldValidating } = this.props.form
 		const mirrorProps = getFieldProps('mirror', {
 			rules: [
-				{validator: this.checkMirror}
+				{ validator: this.checkMirror }
 			]
 		})
 		const approveProps = getFieldProps('approve', {
 			rules: [
-				{validator: this.checkApprove}
+				{ validator: this.checkApprove }
 			]
 		})
 		const extendProps = getFieldProps('extend', {
 			rules: [
-				{validator: this.checkExtend}
+				{ validator: this.checkExtend }
 			]
 		})
 		return (
-				<div className="mirrorservice">
-					<div className="title">
-						镜像服务
+			<div className="mirrorservice">
+				<div className="title">
+					镜像服务
 						<span className="tips">Tips：时速云官方不支持企业版Lite配置私有的镜像仓库，如有需要请联系时速云购买企业版Pro</span>
-					</div>
-					<div className="content">
-						<div className="contentMain">
-							<div className="contentImg">
-								<img src={MirrorImg} alt="镜像服务"/>
-							</div>
-							<div className="contentkeys">
-								<div className="key">镜像服务地址</div>
-								<div className="key">认证服务地址</div>
-								<div className="key">扩展服务地址</div>
-							</div>
-							<div className="contentForm">
-								<Form horizontal className="contentFormMain">
-									<FormItem hasFeedback>
-										<Input {...mirrorProps} placeholder="如：192.168.1.113" disabled={mirrorDisable}/>
-									</FormItem>
-									<FormItem hasFeedback>
-										<Input {...approveProps} placeholder="如：https://192.168.1.113:5001" disabled={mirrorDisable}/>
-									</FormItem>
-									<FormItem hasFeedback>
-										<Input {...extendProps} placeholder="如：https://192.168.1.113:4081" disabled={mirrorDisable}/>
-									</FormItem>
-									<FormItem>
-										{
-											mirrorDisable
-													? <Button type='primary' className="itemInputLeft" onClick={this.handleMirror}>编辑</Button>
-													: ([
+				</div>
+				<div className="content">
+					<div className="contentMain">
+						<div className="contentImg">
+							<img src={MirrorImg} alt="镜像服务" />
+						</div>
+						<div className="contentkeys">
+							<div className="key">镜像服务地址</div>
+							<div className="key">认证服务地址</div>
+							<div className="key">扩展服务地址</div>
+						</div>
+						<div className="contentForm">
+							<Form horizontal className="contentFormMain">
+								<FormItem hasFeedback>
+									<Input {...mirrorProps} placeholder="如：192.168.1.113" disabled={mirrorDisable} />
+								</FormItem>
+								<FormItem hasFeedback>
+									<Input {...approveProps} placeholder="如：https://192.168.1.113:5001" disabled={mirrorDisable} />
+								</FormItem>
+								<FormItem hasFeedback>
+									<Input {...extendProps} placeholder="如：https://192.168.1.113:4081" disabled={mirrorDisable} />
+								</FormItem>
+								<FormItem>
+									{
+										mirrorDisable
+											? <Button type='primary' className="itemInputLeft" onClick={this.handleMirror}>编辑</Button>
+											: ([
 												<Button type='primary' className="itemInputLeft" onClick={this.handleMirror}>保存</Button>,
 												<Button onClick={this.handleReset}>取消</Button>
 											])
-										}
-									</FormItem>
-								</Form>
-							</div>
+									}
+								</FormItem>
+							</Form>
 						</div>
 					</div>
 				</div>
+			</div>
 		)
 	}
 })
@@ -344,8 +344,8 @@ let StorageService = React.createClass({
 		this.props.cephChange()
 	},
 	// 存储节点校验规则
-	checkNode(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkNode(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -362,8 +362,8 @@ let StorageService = React.createClass({
 	},
 
 	// ceph Url 校验规则
-	checkUrl(rule, value, callback){
-		const {validateFields} = this.props.form
+	checkUrl(rule, value, callback) {
+		const { validateFields } = this.props.form
 		if (!value) {
 			callback([new Error('请填写密码')])
 			return
@@ -379,59 +379,59 @@ let StorageService = React.createClass({
 		callback()
 	},
 	render() {
-		const {cephDisable, cephChange} = this.props
-		const {getFieldProps, getFieldError, isFieldValidating} = this.props.form
+		const { cephDisable, cephChange } = this.props
+		const { getFieldProps, getFieldError, isFieldValidating } = this.props.form
 		const nodeProps = getFieldProps('node', {
 			rules: [
-				{validator: this.checkNode}
+				{ validator: this.checkNode }
 			]
 		})
 		const urlProps = getFieldProps('url', {
 			rules: [
-				{validator: this.checkUrl}
+				{ validator: this.checkUrl }
 			]
 		})
 		return (
-				<div className="storageservice">
-					<div className="title">
-						存储服务
+			<div className="storageservice">
+				<div className="title">
+					存储服务
 						<span className="tips">Tips：时速云官方不支持企业版Lite配置存储服务，如有需要请联系时速云购买企业版Pro</span>
-					</div>
-					<div className="content">
-						<div className="contentHeader">
-							Ceph分布式存储
+				</div>
+				<div className="content">
+					<div className="contentHeader">
+						Ceph分布式存储
 						</div>
-						<div className="contentMain">
-							<div className="contentImg">
-								<img src={CephImg} alt="镜像服务"/>
-							</div>
-							<div className="contentkeys">
-								<div className="key">存储节点</div>
-								<div className="key">Ceph URL</div>
-							</div>
-							<div className="contentForm">
-								<Form horizontal className="contentFormMain">
-									<FormItem hasfeedback>
-										<Input {...nodeProps} placeholder="如：192.168.1.113:4081" disabled={cephDisable}/>
-									</FormItem>
-									<FormItem hasfeedback>
-										<Input {...urlProps} placeholder="如：https://192.168.88.6789" disabled={cephDisable}/>
-									</FormItem>
-									<FormItem>
-										{
-											cephDisable
-													? <Button type='primary' className="itemInputLeft" onClick={this.handleCeph}>编辑</Button>
-													: ([
+					<div className="contentMain">
+						<div className="contentImg">
+							<img src={CephImg} alt="镜像服务" />
+						</div>
+						<div className="contentkeys">
+							<div className="key">存储节点</div>
+							<div className="key">Ceph URL</div>
+						</div>
+						<div className="contentForm">
+							<Form horizontal className="contentFormMain">
+								<FormItem hasfeedback>
+									<Input {...nodeProps} placeholder="如：192.168.1.113:4081" disabled={cephDisable} />
+								</FormItem>
+								<FormItem hasfeedback>
+									<Input {...urlProps} placeholder="如：https://192.168.88.6789" disabled={cephDisable} />
+								</FormItem>
+								<FormItem>
+									{
+										cephDisable
+											? <Button type='primary' className="itemInputLeft" onClick={this.handleCeph}>编辑</Button>
+											: ([
 												<Button type='primary' className="itemInputLeft" onClick={this.handleCeph}>保存</Button>,
 												<Button className="itemInputLeft" onClick={this.handleReset}>取消</Button>
 											])
-										}
-									</FormItem>
-								</Form>
-							</div>
+									}
+								</FormItem>
+							</Form>
 						</div>
 					</div>
 				</div>
+			</div>
 		)
 	}
 })
@@ -454,33 +454,33 @@ export default class GlobalConfig extends Component {
 	}
 
 	emailChange() {
-		this.setState({emailDisable: !this.state.emailDisable})
+		this.setState({ emailDisable: !this.state.emailDisable })
 	}
 
 	cicdeditChange() {
-		this.setState({cicdeditDisable: !this.state.cicdeditDisable})
+		this.setState({ cicdeditDisable: !this.state.cicdeditDisable })
 	}
 
 	mirrorChange() {
-		this.setState({mirrorDisable: !this.state.mirrorDisable})
+		this.setState({ mirrorDisable: !this.state.mirrorDisable })
 	}
 
 	cephChange() {
-		this.setState({cephDisable: !this.state.cephDisable})
+		this.setState({ cephDisable: !this.state.cephDisable })
 	}
 
 	render() {
-		const {emailDisable, emailChange, cicdeditDisable, cicdeditChange, mirrorDisable, mirrorChange, cephDisable, cephChange} = this.state
+		const { emailDisable, emailChange, cicdeditDisable, cicdeditChange, mirrorDisable, mirrorChange, cephDisable, cephChange } = this.state
 		return (
-				<div id="GlobalConfig">
-					<div className="alertRow" style={{margin: 0}}>
-						全局配置---对这整个系统的邮件报警、持续集成、镜像服务、分布式存储的配置；只有做了这些配置才能完整的使用这几项功能，分别是：邮件报警对应的是系统中涉及的邮件提醒、持续集成对应的是CI/CD中整个Tenxflow功能的使用、镜像服务对应的是镜像仓库的使用、分布式存储对应的是容器有状态服务存储的使用
+			<div id="GlobalConfig">
+				<div className="alertRow" style={{ margin: 0 }}>
+					全局配置---对这整个系统的邮件报警、持续集成、镜像服务、分布式存储的配置；只有做了这些配置才能完整的使用这几项功能，分别是：邮件报警对应的是系统中涉及的邮件提醒、持续集成对应的是CI/CD中整个Tenxflow功能的使用、镜像服务对应的是镜像仓库的使用、分布式存储对应的是容器有状态服务存储的使用
 					</div>
-					<Emaill emailDisable={emailDisable} emailChange={this.emailChange.bind(this)}/>
-					<ConInter cicdeditDisable={cicdeditDisable} cicdeditChange={this.cicdeditChange.bind(this)}/>
-					<MirrorService mirrorDisable={mirrorDisable} mirrorChange={this.mirrorChange.bind(this)}/>
-					<StorageService cephDisable={cephDisable} cephChange={this.cephChange.bind(this)}/>
-				</div>
+				<Emaill emailDisable={emailDisable} emailChange={this.emailChange.bind(this)} />
+				<ConInter cicdeditDisable={cicdeditDisable} cicdeditChange={this.cicdeditChange.bind(this)} />
+				<MirrorService mirrorDisable={mirrorDisable} mirrorChange={this.mirrorChange.bind(this)} />
+				<StorageService cephDisable={cephDisable} cephChange={this.cephChange.bind(this)} />
+			</div>
 		)
 	}
 }
