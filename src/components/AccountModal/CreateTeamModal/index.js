fd894422b5194ac86231f14b9362cd9cc0512328
yubiao <yubiao@tenxcloud.com>
2017-03-18 17:06:07 +0800

    更改提示信息

--- src/components/AccountModal/CreateTeamModal/index.js
+++ src/components/AccountModal/CreateTeamModal/index.js
@@ -34,7 +34,7 @@ let CreateTeamModal = React.createClass({
     }
     const { checkTeamName } = this.props.funcs
     if (!USERNAME_REG_EXP_NEW.test(value)) {
-      callback(new Error('以[a~z]开头，允许[0~9]、[-]，长度大于3，且以小写英文和数字结尾'))
+      callback(new Error('以[a~z]开头，允许[0~9]、[-]，长度大于4，且以小写英文和数字结尾'))
       return
     }
     
--- src/components/ServiceConfig/CreateConfigModal.js
+++ src/components/ServiceConfig/CreateConfigModal.js
@@ -62,7 +62,7 @@ let CreateConfigModal = React.createClass({
       return
     }
     if (!USERNAME_REG_EXP_NEW.test(value)) {
-      callback(new Error('以[a~z]开头，允许[0~9]、[-]，长度大于3，且以小写英文和数字结尾'))
+      callback(new Error('以[a~z]开头，允许[0~9]、[-]，长度大于4，且以小写英文和数字结尾'))
       return
     }
     callback()
