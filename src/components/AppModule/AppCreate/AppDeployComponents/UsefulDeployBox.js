a5d8319d7d7f2d78b71f525e612522f5e412ba22
yubiao <yubiao@tenxcloud.com>
2017-03-28 17:06:54 +0800

    fix issue

--- src/components/AppModule/AppCreate/AppDeployComponents/UsefulDeployBox.js
+++ src/components/AppModule/AppCreate/AppDeployComponents/UsefulDeployBox.js
@@ -18,20 +18,167 @@ const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
 
 let UsefulDeployBox = React.createClass({
-  render:function () {
+  checkLivePort(rule, value, callback) {
+    if (!value) {
+      return callback('请填写端口')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0 || value > 65535) {
+      return callback('请填写正确端口')
+    }
+    return callback()
+  },
+  checkDelay(rule, value, callback) {
+    if (!value) {
+      return callback('请填写首次延迟时间')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('延迟时间需大于1')
+    }
+    return callback()
+  },
+  checkTimeout(rule, value, callback) {
+    if (!value) {
+      return callback('请填超时时间')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('超时时间需大于1')
+    }
+    return callback()
+  },
+  checkPeriod(rule, value, callback) {
+    if (!value) {
+      return callback('请填写检查间隔')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('检查间隔事件需大于1')
+    }
+    return callback()
+  },
+  checkPath(rule, value, callback) {
+    if (!value) {
+      return callback('请填写Path路径')
+    }
+    return callback()
+  },
+  checkTCPTimeout(rule, value, callback) {
+    if (!value) {
+      return callback('请填超时时间')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('超时时间需大于1')
+    }
+    return callback()
+  },
+  checkTCPLivePort(rule, value, callback) {
+    if (!value) {
+      return callback('请填写端口')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0 || value > 65535) {
+      return callback('请填写正确端口')
+    }
+    return callback()
+  },
+  checkTCPDelay(rule, value, callback) {
+    if (!value) {
+      return callback('请填写首次延迟时间')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('延迟时间需大于1')
+    }
+    return callback()
+  },
+  checkTCPPeriod(rule, value, callback) {
+    if (!value) {
+      return callback('请填写首次延迟时间')
+    }
+    if (!/[0-9]{1,5}/.test(value)) {
+      return callback('请填写数字')
+    }
+    value = parseInt(value)
+    if (value < 0) {
+      return callback('延迟时间需大于1')
+    }
+    return callback()
+  },
+  render: function () {
     const { form } = this.props
     const parentScope = this.props.scope;
-    const { getFieldProps, getFieldError, isFieldValidating ,getFieldValue} = form
+    const { getFieldProps, getFieldError, isFieldValidating, getFieldValue } = form
     const runningCodeProps = getFieldProps('imageUrl', {});
-    const httpLivePort = getFieldProps('httpLivePort', {});
-    const httpLiveInitialDelaySeconds = getFieldProps('httpLiveInitialDelaySeconds', {});
-    const httpLiveTimeoutSeconds = getFieldProps('httpLiveTimeoutSeconds', { });
-    const httpLivePeriodSeconds = getFieldProps('httpLivePeriodSeconds', {});
-    const httpLivePath = getFieldProps('httpLivePath', {});
-    const tcpLivePort = getFieldProps('tcpLivePort', {});
-    const tcpLiveInitialDelaySeconds = getFieldProps('tcpLiveInitialDelaySeconds', {});
-    const tcpLiveTimeoutSeconds = getFieldProps('tcpLiveTimeoutSeconds', {});
-    const tcpLivePeriodSeconds = getFieldProps('tcpLivePeriodSeconds', {});
+    const protocol = getFieldValue('getUsefulType')
+    const httpLivePort = getFieldProps('httpLivePort', {
+      rules: [{
+        validator: protocol == 'http' ? this.checkLivePort : ''
+      }]
+    });
+    const httpLiveInitialDelaySeconds = getFieldProps('httpLiveInitialDelaySeconds', {
+      rules: [{
+        validator: protocol == 'http' ? this.checkDelay : ''
+      }]
+    });
+    const httpLiveTimeoutSeconds = getFieldProps('httpLiveTimeoutSeconds', {
+      rules: [{
+        validator: protocol == 'http' ? this.checkTimeout : ''
+      }]
+    });
+    const httpLivePeriodSeconds = getFieldProps('httpLivePeriodSeconds', {
+      rules: [{
+        validator: protocol == 'http' ? this.checkPeriod : ''
+      }]
+    });
+    const httpLivePath = getFieldProps('httpLivePath', {
+      rules: [{
+        validator: protocol == 'http' ? this.checkPath : ''
+      }]
+    });
+    const tcpLivePort = getFieldProps('tcpLivePort', {
+      rules: [{
+        validator: protocol == 'tcp' ? this.checkTCPLivePort : ''
+      }]
+    });
+    const tcpLiveInitialDelaySeconds = getFieldProps('tcpLiveInitialDelaySeconds', {
+      rules: [{
+        validator: protocol == 'tcp' ? this.checkTCPDelay : ''
+      }]
+    });
+    const tcpLiveTimeoutSeconds = getFieldProps('tcpLiveTimeoutSeconds', {
+      rules: [{
+        validator: protocol == 'tcp' ? this.checkTCPTimeout : ''
+      }]
+    });
+    const tcpLivePeriodSeconds = getFieldProps('tcpLivePeriodSeconds', {
+      rules: [{
+        validator: protocol == 'tcp' ? this.checkTCPPeriod : ''
+      }]
+    });
     return (
       <div id="UsefulDeployBox">
         <div className="usefulBox">
@@ -40,13 +187,14 @@ let UsefulDeployBox = React.createClass({
               {...getFieldProps('getUsefulType', {
                 initialValue: 'null',
               }) }
-              >
+            >
               <Radio key="a" value={"null"}>无</Radio>
               <Radio key="b" value={"http"}>HTTP</Radio>
               <Radio key="c" value={"tcp"}>TCP</Radio>
             </RadioGroup>
           </FormItem>
-          { getFieldValue('getUsefulType') == "http" ? [
+
+          {getFieldValue('getUsefulType') == "http" ? [
             <div className="http" key="http">
               <div className="title">
                 <div className="httpcommonTitle">
@@ -65,16 +213,24 @@ let UsefulDeployBox = React.createClass({
               </div>
               <div className="input">
                 <div className="commonInput">
-                  <Input type="text" size="large" {...httpLivePort} />
+                  <FormItem>
+                    <Input type="text" size="large" {...httpLivePort} />
+                  </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...httpLiveInitialDelaySeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...httpLiveInitialDelaySeconds} />&nbsp;&nbsp;s
+                </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...httpLiveTimeoutSeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...httpLiveTimeoutSeconds} />&nbsp;&nbsp;s
+                   </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...httpLivePeriodSeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...httpLivePeriodSeconds} />&nbsp;&nbsp;s
+                    </FormItem>
                 </div>
                 <div style={{ clear: "both" }}></div>
               </div>
@@ -87,13 +243,15 @@ let UsefulDeployBox = React.createClass({
               <div className="input">
                 <span style={{ float: "left", marginLeft: "10px" }}>/</span>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...httpLivePath} />
+                  <FormItem>
+                    <Input type="text" size="large" {...httpLivePath} />
+                  </FormItem>
                 </div>
                 <div style={{ clear: "both" }}></div>
               </div>
             </div>
           ] : null}
-          { getFieldValue('getUsefulType') == "tcp" ? [
+          {getFieldValue('getUsefulType') == "tcp" ? [
             <div className="tcp" key="tcp">
               <div className="title">
                 <div className="tcpcommonTitle">
@@ -112,16 +270,24 @@ let UsefulDeployBox = React.createClass({
               </div>
               <div className="input">
                 <div className="commonInput">
-                  <Input type="text" size="large" {...tcpLivePort} />
+                  <FormItem>
+                    <Input type="text" size="large" {...tcpLivePort} />
+                  </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...tcpLiveInitialDelaySeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...tcpLiveInitialDelaySeconds} />&nbsp;&nbsp;s
+                    </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...tcpLiveTimeoutSeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...tcpLiveTimeoutSeconds} />&nbsp;&nbsp;s
+                     </FormItem>
                 </div>
                 <div className="commonInput">
-                  <Input type="text" size="large" {...tcpLivePeriodSeconds} />&nbsp;&nbsp;s
+                  <FormItem>
+                    <Input type="text" size="large" {...tcpLivePeriodSeconds} />&nbsp;&nbsp;s
+                    </FormItem>
                 </div>
                 <div style={{ clear: "both" }}></div>
               </div>
