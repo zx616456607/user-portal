cc470d9ec508ece146fc068e02f56cc5519f0c7b
zhangchengzheng <zhangchengzheng@tenxcloud.com>
2017-03-29 15:20:31 +0800

    存储服务item互换位置

--- src/components/SettingModal/GlobalConfig/index.js
+++ src/components/SettingModal/GlobalConfig/index.js
@@ -844,16 +844,16 @@ let StorageService = React.createClass({
               <img src={CephImg} alt="镜像服务" />
             </div>
             <div className="contentkeys">
-              <div className="key">存储节点</div>
               <div className="key">Ceph URL</div>
+              <div className="key">存储节点</div>
             </div>
             <div className="contentForm">
               <Form horizontal className="contentFormMain">
                 <FormItem >
-                  <Input {...nodeProps} placeholder="如：192.168.1.113:4081，如有多个存储节点，请使用英文逗号隔开" disabled={cephDisable} />
+                  <Input {...urlProps} placeholder="如：https://192.168.88.6789" disabled={cephDisable} />
                 </FormItem>
                 <FormItem >
-                  <Input {...urlProps} placeholder="如：https://192.168.88.6789" disabled={cephDisable} />
+                  <Input {...nodeProps} placeholder="如：192.168.1.113:4081，如有多个存储节点，请使用英文逗号隔开" disabled={cephDisable} />
                 </FormItem>
                 <FormItem>
                   {
