51f6b7e88411f4c8c0c9183ba6c06417b226af5c
Watermelon <watermelon@AmosdeMacBook-Pro.local>
2017-04-02 16:22:56 +0800

    fix issue

--- src/components/CICDModule/CodeStore/index.js
+++ src/components/CICDModule/CodeStore/index.js
@@ -81,8 +81,6 @@ const MyComponent = React.createClass({
   operaMenuClick: function (item, e) {
     //this function for user click the dropdown menu
     const key = e.key.split('@')[0]
-    console.log(key)
-    console.log(e)
     switch (key) {
       case '1':
         this.setState({
