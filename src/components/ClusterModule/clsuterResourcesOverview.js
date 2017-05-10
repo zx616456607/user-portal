ae8b1791417a44badd047f9fd42dc58b0a0f2dfe
yubiao <yubiao@tenxcloud.com>
2017-05-10 10:00:50 +0800

    fix issue

--- src/components/ClusterModule/clsuterResourcesOverview.js
+++ src/components/ClusterModule/clsuterResourcesOverview.js
@@ -158,7 +158,7 @@ class ClusterResourcesOverview extends Component{
               </li>
               <li>
                 <span className="itemKey ready">已分配数</span>
-                <span>{resource ? `${resource.allocatedCPU} 核` : NOT_AVAILABLE}</span>
+                <span>{resource ? `${resource.allocatedCPU.toFixed(2)} 核` : NOT_AVAILABLE}</span>
               </li>
               <li>
                 <span className="itemKey success">实际使用</span>
