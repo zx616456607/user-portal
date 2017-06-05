90bf0b702c332270bd9f7c44ca645a9100accd80
lizhen <lizhen@tenxcloud.com>
2017-06-05 22:17:09 +0800

    wrap harbor native api

--- registry/lib/harborAPIs.js
+++ registry/lib/harborAPIs.js
@@ -48,6 +48,90 @@ HarborAPIs.prototype.searchProjects = function (query, callback) {
   this.sendRequest(requestUrl, 'GET', null, callback);
 };
 
+// [GET] /jobs/replication
+HarborAPIs.prototype.getReplicationJobs = function (query, callback) {
+  const url = `${this.getAPIPrefix()}/jobs/replication${encodeQueryString(query)}`
+  this.sendRequest(url, 'GET', null, callback)
+}
+
+// [DELETE] /jobs/replication/{id}
+HarborAPIs.prototype.deleteReplicationJob = function (id, callback) {
+  const url = `${this.getAPIPrefix()}/jobs/replication/${id}`
+  this.sendRequest(url, 'DELETE', null, callback)
+}
+
+// [GET] /jobs/replication/{id}/log
+HarborAPIs.prototype.getReplicationJobLogs = function (callback) {
+  const url = `${this.getAPIPrefix()}/jobs/replication/${id}/log`
+  this.sendRequest(url, 'GET', null, callback)
+}
+
+// [GET] /policies/replication
+HarborAPIs.prototype.getReplicationPolicies = function (query, callback) {
+  const url = `${this.getAPIPrefix()}/policies/replication${encodeQueryString(query)}`
+  this.sendRequest(url, 'GET', null, callback)
+}
+
+// [POST] /policies/replication
+HarborAPIs.prototype.newReplicationPolicy = function (policy, callback) {
+  const method = "newReplicationPolicy"
+}
+
+// [GET] /policies/replication/{id}
+HarborAPIs.prototype.getReplicationPolicy = function (callback) {
+  const method = "getReplicationPolicy"
+}
+
+// [PUT] /policies/replication/{id}
+HarborAPIs.prototype.modifyReplicationPolicy = function (policy, callback) {
+  const method = "modifyReplicationPolicy"
+}
+
+// [PUT] /policies/replication/{id}/enablement
+HarborAPIs.prototype.enableReplicationPolicy = function (enable, callback) {
+  const method = "enableReplicationPolicy"
+}
+
+// [GET] /targets
+HarborAPIs.prototype.getReplicationTargets = function (query, callback) {
+  const method = "getReplicationTargets"
+}
+
+// [POST] /targets
+HarborAPIs.prototype.newReplicationTarget = function (target, callback) {
+  const method = "newReplicationTarget"
+}
+
+// [POST] /targets/ping
+HarborAPIs.prototype.pingReplicationTarget = function (target, callback) {
+  const method = "pingReplicationTarget"
+}
+
+// [POST] /targets/{id}/ping
+HarborAPIs.prototype.pingReplicationTargetByID = function (callback) {
+  const method = "pingReplicationTargetByID"
+}
+
+// [PUT] /targets/{id}
+HarborAPIs.prototype.modifyReplicationTarget = function (target, callback) {
+  const method = "modifyReplicationTarget"
+}
+
+// [GET] /targets/{id}
+HarborAPIs.prototype.getReplicationTarget = function (callback) {
+  const method = "getReplicationTarget"
+}
+
+// [DELETE] /targets/{id}
+HarborAPIs.prototype.deleteReplicationTarget = function (callback) {
+  const method = "deleteReplicationTarget"
+}
+
+// [GET] /targets/{id}/policies
+HarborAPIs.prototype.getReplicationTargetRelatedPolicies = function (callback) {
+  const method = "getReplicationTargetRelatedPolicies"
+}
+
 
 HarborAPIs.prototype.sendRequest = function (requestUrl, httpMethod, data, callback) {
   var method = "sendRequest";
@@ -96,6 +180,11 @@ HarborAPIs.prototype.getAuthorizationHeader = function () {
   return {
     'Authorization': 'Basic ' + this.authInfo
   }
+}
+
+function encodeQueryString(kvs) {
+  return '?' + Object.getOwnPropertyNames(kvs).map(
+    key => `${encodeURIComponent(key)}=${encodeURIComponent(kvs[key])}`).join('&')
 }
 
 module.exports = HarborAPIs;
