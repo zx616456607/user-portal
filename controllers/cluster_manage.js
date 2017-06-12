cb524f0334372a3088d1c8ce3ba0224c7242d5eb
houxz <houxz@tenxcloud>
2017-06-12 19:07:14 +0800

    fix cluster_emoji_name bug

--- controllers/cluster_manage.js
+++ controllers/cluster_manage.js
@@ -333,7 +333,6 @@ exports.getClusterNetworkMode = function* () {
 
 function FilterEmoji(str) {
   var reg = /[^\u4e00-\u9fa5|\u0000-\u00ff|\u3002|\uFF1F|\uFF01|\uff0c|\u3001|\uff1b|\uff1a|\u3008-\u300f|\u2018|\u2019|\u201c|\u201d|\uff08|\uff09|\u2014|\u2026|\u2013|\uff0e]/g;
-  console.log(typeof (str), str)
   str = str.replace(reg, '');
   return str
 }
