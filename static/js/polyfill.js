fb99c6465a3a7b309e18b88a2481dbcf5c6b28ae
zhangpc <zhangpc@tenxcloud.com>
2017-06-19 17:23:18 +0800

    remove uselsess code

--- static/js/polyfill.js
+++ static/js/polyfill.js
@@ -113,36 +113,5 @@ if (!Array.prototype.includes) {
  * Polyfill for `Objec.assign`
  */
 require('es6-object-assign').polyfill()
-/*if (!Object.assign) {
-  Object.defineProperty(Object, 'assign', {
-    enumerable: false,
-    configurable: true,
-    writable: true,
-    value: assign
-  });
-}
-function assign(target, firstSource) {
-  if (target === undefined || target === null) {
-    throw new TypeError('Cannot convert first argument to object');
-  }
-
-  var to = Object(target);
-  for (var i = 1; i < arguments.length; i++) {
-    var nextSource = arguments[i];
-    if (nextSource === undefined || nextSource === null) {
-      continue;
-    }
-
-    var keysArray = Object.keys(Object(nextSource));
-    for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-      var nextKey = keysArray[nextIndex];
-      var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-      if (desc !== undefined && desc.enumerable) {
-        to[nextKey] = nextSource[nextKey];
-      }
-    }
-  }
-  return to;
-}*/
 
 },{"es6-object-assign":1}]},{},[2]);
