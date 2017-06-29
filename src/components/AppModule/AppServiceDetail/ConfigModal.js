0f7df4dfa4e9f31740b490dacb99527eff180c9b
zhangchengzheng <zhangchengzheng@tenxcloud.com>
2017-06-29 14:32:09 +0800

    serviceDetail cpu toString()

--- src/components/AppModule/AppServiceDetail/ConfigModal.js
+++ src/components/AppModule/AppServiceDetail/ConfigModal.js
@@ -47,7 +47,7 @@ function getCPUByMemory(memory) {
     case '8192':
       return '2'
     default:
-      return RESOURCES_CPU_DEFAULT
+      return RESOURCES_CPU_DEFAULT.toString()
   }
 }
 class ConfigModal extends Component {
@@ -88,7 +88,6 @@ class ConfigModal extends Component {
     } else {
       cpu = cpu.toString()
     }
-    cpu = cpu.toString()
     if (cpu.indexOf('m') > -1) {
       cpu = parseInt(cpu)
       cpu /= 1000
