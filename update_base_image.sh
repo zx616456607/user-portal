eba24554a472e856aab6004012400131aef6f307
zhangpc <zhangpc@tenxcloud.com>
2017-07-06 19:54:31 +0800

    change base image to base-6-alpine

--- update_base_image.sh
+++ update_base_image.sh
@@ -3,7 +3,7 @@ set -e
 # docker login
 docker login -e zhangpc@tenxcloud.com -u zhangpc -p 11111111 192.168.1.113
 # build base image
-base_image="192.168.1.113/zhangpc/user-portal:base-4.4.7"
-docker build -t ${base_image} -f Dockerfile.base .
+base_image="192.168.1.113/zhangpc/user-portal:base-6-alpine"
+docker build -t ${base_image} -f Dockerfile.base.alpine .
 # push base image
 docker push ${base_image}
