bf1b286b674f992fa9b92cf164103181f62983e7
zhangxuan <zhangxuan@tenxcloud.com>
2017-07-11 14:59:27 +0800

    change popSelect.js

--- src/components/PopSelect/Content.js
+++ src/components/PopSelect/Content.js
@@ -8,23 +8,18 @@
  * @author Zhangpc
  */
 import React, { Component, PropTypes } from 'react'
-import { Input, Button, Spin, Icon, Select } from 'antd'
+import { Input, Button, Spin, Icon, } from 'antd'
 import './style/Content.less'
 import { MY_SPACE } from '../../constants'
 import classNames from 'classnames'
-const Option = Select.Option;
+
 const mode = require('../../../configs/model').mode
 const standard = require('../../../configs/constants').STANDARD_MODE
-import NotificationHandler from '../../common/notification_handler'
 
 class PopSelect extends Component {
   constructor(props) {
     super(props)
     this.handleSearchInput = this.handleSearchInput.bind(this)
-    this.state={
-      value: '',
-      focus: false,
-    }
   }
 
   componentWillMount() {
@@ -33,50 +28,14 @@ class PopSelect extends Component {
       list,
     })
   }
+
   componentWillReceiveProps(nextProps) {
     const { list } = nextProps
     this.setState({
       list,
     })
   }
-  handleChange(value) {
-    this.setState({ value });
-    const { list } = this.props
-    const notify = new NotificationHandler()
-    let last = [];
-    if(value=="" || (value.indexOf(" ")!=-1)){
-      last = list
-    }else {
-      for(let i = 0;i < list.length;i++){
-        if((list[i].name).indexOf(value)!=-1) {
-          last.push(list[i])
-        }
-      }
-    }
-    this.setState({
-      list:last
-    })
-  }
-  handleSubmit() {
-    let input = document.getElementsByClassName('ant-select-search__field')[0];
-    const { value } = this.state;
-    const { onChange } = this.props;
-    const { list } = this.state;
-    const notify = new NotificationHandler()
-    let space = list.find((item) => { return value === item.spaceName })
-    if (space) {
-      onChange(space)
-      this.setState({ value:'' });
-      input.value = ''
-    } else {
-      notify.info('输入内容有误')
-    }
-  }
-  handleFocusBlur(e) {
-    this.setState({
-      focus: e.target === document.activeElement,
-    });
-  }
+
   handleSearchInput() {
     const searchItem = this.refs.titleInput.refs.input.value
     const { list } = this.props
@@ -122,26 +81,8 @@ class PopSelect extends Component {
       </div>
     )
   }
-  handleSelect(value) {
-    const { onChange } = this.props;
-    let input = document.getElementsByClassName('ant-select-search__field')[0];
-    const { list } = this.state;
-    let space = list.find((item) => { return value === item.spaceName })
-    if (space) {
-      onChange(space)
-      this.setState({ value:'' });
-      input.value = '';
-    }
-  }
+
   render() {
-    const btnCls = classNames({
-      'ant-search-btn': true,
-      'ant-search-btn-noempty': !!this.state.value.trim(),
-    });
-    const searchCls = classNames({
-      'ant-search-input': true,
-      'ant-search-input-focus': this.state.focus,
-    });
     const { onChange, loading, special, popTeamSelect, Search } = this.props
     const { list } = this.state
     let searchList = (
@@ -155,7 +96,6 @@ class PopSelect extends Component {
             disabled= !isOk
           }
           let liProps = {
-            value: item.spaceName,
             key: name,
             className: classNames({
               searchItem: true,
@@ -166,24 +106,14 @@ class PopSelect extends Component {
             liProps.onClick = () => onChange(item)
           }
           return (
-            Search ?
-              <Option {...liProps}>
-                {
-                  popTeamSelect ?
-                    item.teamName
-                    :
-                    item.name
-                }
-              </Option>
-              :
-              <li {...liProps}>
-                {
-                  popTeamSelect ?
-                    item.teamName
-                    :
-                    item.name
-                }
-              </li>
+            <li {...liProps}>
+              {
+                popTeamSelect ?
+                  item.teamName
+                  :
+                  item.name
+              }
+            </li>
           )
         })
     )
@@ -200,44 +130,20 @@ class PopSelect extends Component {
       return (
         <div className="PopSelectContent">
           {this.getSpecial()}
-          <div className="ant-search-input-wrapper">
-            <Input.Group className={searchCls}>
-              <Select
-                combobox
-                value={this.state.value}
-                placeholder="请输入空间名"
-                notFoundContent="结果为空"
-                defaultActiveFirstOption={true}
-                showArrow={true}
-                filterOption={false}
-                onChange={this.handleChange.bind(this)}
-                onFocus={this.handleFocusBlur.bind(this)}
-                onBlur={this.handleFocusBlur.bind(this)}
-                onSelect={this.handleSelect.bind(this)}
-              >
-                {searchList}
-              </Select>
-              <div className="ant-input-group-wrap">
-                <Button className={btnCls}>
-                  <Icon type="search" onClick={this.handleSubmit.bind(this)}/>
-                </Button>
-              </div>
-            </Input.Group>
-          </div>
-          {/*<span className='titlesearch'>*/}
-            {/*<Input*/}
-              {/*placeholder="请输入空间名"*/}
-              {/*size="large"*/}
-              {/*ref='titleInput'*/}
-              {/*id='titleInput'*/}
-              {/*onChange={this.handleSearchInput}*/}
-              {/*onPressEnter={this.handleSearchInput}*/}
-            {/*/>*/}
-            {/*<Icon type="search" className='titleicon' onClick={this.handleSearchInput}/>*/}
-          {/*</span>*/}
-          {/*<ul className="searchList">*/}
-            {/*{searchList}*/}
-          {/*</ul>*/}
+          <span className='titlesearch'>
+            <Input
+              placeholder="请输入空间名"
+              size="large"
+              ref='titleInput'
+              id='titleInput'
+              onChange={this.handleSearchInput}
+              onPressEnter={this.handleSearchInput}
+            />
+            <Icon type="search" className='titleicon' onClick={this.handleSearchInput}/>
+          </span>
+          <ul className="searchList">
+            {searchList}
+          </ul>
         </div>
       )
     }else{
