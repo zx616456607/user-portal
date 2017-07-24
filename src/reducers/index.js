51f79ad85219519762dc616dfc1450f7c64d4cbc
zhangchengzheng <zhangchengzheng@tenxcloud.com>
2017-07-24 15:03:07 +0800

    角色管理

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -47,6 +47,7 @@ import ldap from './ldap'
 import harbor from './harbor'
 import notifications from './notifications'
 import projectAuthority from './project'
+import role from './role'
 
 import { LOGIN_EXPIRED_MESSAGE, PAYMENT_REQUIRED_CODE, UPGRADE_EDITION_REQUIRED_CODE, } from '../constants'
 
@@ -156,6 +157,7 @@ const rootReducer = combineReducers({
   harbor,
   notifications,
   projectAuthority,
+  role,
 })
 
 export default rootReducer
