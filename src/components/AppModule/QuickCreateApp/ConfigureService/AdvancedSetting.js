2b58d756dfc02e9b33c9a659752a76e48e12664d
zhangchengzheng <zhangchengzheng@tenxcloud.com>
2017-08-04 19:46:06 +0800

    @fix issue CRYSTAL-4605: 创建服务添加环境变量时，需要校验env的值是否为空，给出提示，并且不允许创建服务

--- src/components/AppModule/QuickCreateApp/ConfigureService/AdvancedSetting.js
+++ src/components/AppModule/QuickCreateApp/ConfigureService/AdvancedSetting.js
@@ -21,6 +21,20 @@ const Panel = Collapse.Panel
 const FormItem = Form.Item
 
 const AdvancedSetting = React.createClass({
+  getInitialState(){
+    return {
+      activeKey: null,
+    }
+  },
+  componentWillReceiveProps(nextProps){
+    let nextKey = nextProps.AdvancedSettingKey
+    let currentKey = this.props.AdvancedSettingKey
+    if((nextKey == '1' || currentKey == '1') && this.state.activeKey !== '1'){
+      this.setState({
+        activeKey: '1'
+      })
+    }
+  },
   addEnvKey() {
     const { form } = this.props
     const { setFieldsValue, getFieldValue, validateFieldsAndScroll } = form
@@ -87,14 +101,14 @@ const AdvancedSetting = React.createClass({
     const envValueKey = `envValue${keyValue}`
     const envNameProps = getFieldProps(envNameKey, {
       rules: [
-        // { required: true, message: '请填写键' },
+        { required: true, message: '请填写键' },
         { validator: this.checkEnv },
       ],
     })
     const envValueProps = getFieldProps(envValueKey, {
-      // rules: [
-      //   { required: true, message: '请填写值' }
-      // ],
+       rules: [
+         { required: true, message: '请填写值' }
+       ],
     })
     return (
       <Row className="configItem" key={`configItem${keyValue}`}>
@@ -123,6 +137,11 @@ const AdvancedSetting = React.createClass({
       </Row>
     )
   },
+  collapseChange(key){
+    this.setState({
+      activeKey: key
+    })
+  },
   render() {
     const { formItemLayout, form } = this.props
     const { getFieldValue } = form
@@ -142,8 +161,8 @@ const AdvancedSetting = React.createClass({
     )
     return (
       <div id="advancedConfigureService">
-        <Collapse>
-          <Panel header={header}>
+        <Collapse onChange={this.collapseChange} activeKey={this.state.activeKey}>
+          <Panel header={header} key="1">
             <Row>
               <Col span={formItemLayout.labelCol.span} className="formItemLabel">
                 环境变量
