cfe2611f3740ea3b02e825073ad7eedddba718bc
zhangxuan <zhangxuan@tenxcloud.com>
2017-09-30 11:59:36 +0800

    CRYSTAL-5182 jira link:http://jira.tenxcloud.com/browse/CRYSTAL-5182

--- src/components/AppModule/AutoScale/AutoScaleModal/index.js
+++ src/components/AppModule/AutoScale/AutoScaleModal/index.js
@@ -192,6 +192,9 @@ class AutoScaleModal extends React.Component {
     if (!value) {
       return callback('请输入策略名称')
     }
+    if (value.length < 3 || value.length > 21) {
+      return callback('策略名称长度在3-21位之间')
+    }
     if (!isEmpty(scaleDetail)) {
       if (getFieldValue('serviceName') === scaleDetail.serviceName) {
         return callback()
