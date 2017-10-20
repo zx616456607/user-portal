bae6640ce7cf60f662e8e8f3f9f3a2ab13cb3ba7
zhaoyanbei <727114935@qq.com>
2017-10-20 11:29:07 +0800

    fix issue CRYSTAL-5362

--- src/components/DatabaseCache/RedisCluster.js
+++ src/components/DatabaseCache/RedisCluster.js
@@ -212,7 +212,7 @@ class RedisDatabase extends Component {
             <Tooltip title={title} placement="right"><Button type='primary' size='large' onClick={this.createDatabaseShow} disabled={!canCreate}>
               <i className='fa fa-plus' />&nbsp;Redis集群
           </Button></Tooltip>
-            <Button style={{marginLeft:'20px',padding:'5px 15px'}} size='large' onClick={this.clusterRefresh} disabled={!canCreate}>
+            <Button className="button_refresh" size='large' onClick={this.clusterRefresh} disabled={!canCreate}>
               <i className='fa fa-refresh' />&nbsp;刷 新
             </Button>
             <span className='rightSearch'>
