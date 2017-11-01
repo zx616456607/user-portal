386f121f0c0be4b1463c762a7a0d45c4734e8ab1
zhaoyanbei <727114935@qq.com>
2017-11-01 14:37:13 +0800

    fix issue CRYSTAL-5621

--- src/components/TenantManage/RoleManagement/index.js
+++ src/components/TenantManage/RoleManagement/index.js
@@ -54,6 +54,7 @@ class RoleManagement extends React.Component {
       autoExpandParent: true,
       creationTime: true,
       total: 0,
+      current: 0,
     }
   }
 
@@ -84,7 +85,7 @@ class RoleManagement extends React.Component {
     let query = {
       from: page * 10,
       size: 10,
-      sort
+      sort,
     }
     ListRole(query, {
       success: {
@@ -94,7 +95,8 @@ class RoleManagement extends React.Component {
             this.setState({
               loading: false,
               roleData: data,
-              total: res.data.data.total
+              current: n === undefined ? 1 : n,
+              total: res.data.data.total,
             })
           }
         },
@@ -344,7 +346,9 @@ class RoleManagement extends React.Component {
    */
   handleSearch(data) {
     this.setState({
-      roleData: data ? data.items : []
+      roleData: data ? data.items : [],
+      total: data.total,
+      current: 1,
     })
   }
   handleSort() {
@@ -369,6 +373,7 @@ class RoleManagement extends React.Component {
       total: total,
       defaultPageSize: 10,
       defaultCurrent: 1,
+      current: this.state.current,
       onChange: (n) => this.loadData(null, n)
     };
     const columns = [{
@@ -498,7 +503,6 @@ class RoleManagement extends React.Component {
       return <TreeNode key={item.id} title={item.name} disableCheckbox />;
     })
     const scope = this
-
     return (
       <QueueAnim className="RoleManagement">
         <div id="RoleManagement" key='RoleManagement'>
