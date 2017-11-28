96206f34a7f74d734c4438d6fe709d4c10b1f18e
zhangpc <zhangpc@tenxcloud.com>
2017-11-28 19:53:14 +0800

    fix issue #CRYSTAL-5804

--- src/components/AppModule/AppServiceDetail/AppServiceDetailInfo.js
+++ src/components/AppModule/AppServiceDetail/AppServiceDetailInfo.js
@@ -1009,10 +1009,14 @@ class AppServiceDetailInfo extends Component {
         Object.assign(list[currentIndex], values)
       } else {
         const lastVolume = list[list.length - 1]
-        const { volumesName } = lastVolume
-        const vloumeArray = volumesName.split('-')
-        const index = parseInt(vloumeArray[1]) + 1
-        values.volumesName = `volume-${index}`
+        if (lastVolume) {
+          const { volumesName } = lastVolume
+          const vloumeArray = volumesName.split('-')
+          const index = parseInt(vloumeArray[1]) + 1
+          values.volumesName = `volume-${index}`
+        } else {
+          values.volumesName = `volume-0`
+        }
         list.push(values)
       }
       this.setState({
