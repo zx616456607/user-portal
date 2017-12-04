0a1936da2fa2df3d419cf28e5670a47e2f187237
zhangxuan <zhangxuan@tenxcloud.com>
2017-12-04 19:38:24 +0800

    update isHarborManager

--- src/components/AppCenter/ImageCenter/ItemDetail.js
+++ src/components/AppCenter/ImageCenter/ItemDetail.js
@@ -101,7 +101,7 @@ class ItemDetail extends Component {
       this.currentUser = currentMember
     }
     const currentUserRole = currentMember[camelize('role_id')]
-    let isAdminAndHarbor = (isAdmin && role === ROLE_SYS_ADMIN) || (currentMember === 1)
+    let isAdminAndHarbor = (isAdmin && role === ROLE_SYS_ADMIN) || (currentUserRole === 1)
     const tabPanels = [
       <TabPane tab="镜像仓库" key="repo">
         <CodeRepo registry={DEFAULT_REGISTRY} {...this.props} isAdminAndHarbor={isAdminAndHarbor}/>
