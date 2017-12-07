69e141ff91f82d8064d4ddbb3473cea636c9b477
zhangpc <zhangpc@tenxcloud.com>
2017-12-07 19:29:00 +0800

    fix issue #CRYSTAL-5896

--- src/components/AppCenter/ImageCenter/ProjectDetail/ImageVersion.js
+++ src/components/AppCenter/ImageCenter/ProjectDetail/ImageVersion.js
@@ -390,6 +390,7 @@ class ImageVersion extends Component {
         </div>
         <Modal title="镜像版本详情" visible={this.state.detailVisible} style={{ paddingRight: 5 }}
           onCancel={this.handleClose.bind(this)}
+          wrapClassName="image-detail-modal"
           footer={[
             <Button key="back" type="primary" size="large" onClick={this.handleClose.bind(this)}>知道了</Button>,
           ]}>
