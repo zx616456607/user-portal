960d092186d93a9c90d60f60fefe29998b913bc4
zhangxuan <zhangxuan@tenxcloud.com>
2017-12-28 15:56:14 +0800

    EV-335 jira link:http://jira.tenxcloud.com/browse/EV-335

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -579,7 +579,7 @@ class App extends Component {
             <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>
             <div className="alert_text">
               超过配额，你目前只剩下
-            <a> {resourcequotaMessage.available >= 0 ? resourcequotaMessage.available.toFixed(2) : 0} {this.quotaSuffix(resourcequotaMessage.type)} {this.quotaEn(resourcequotaMessage.type)} </a>
+            <a> {resourcequotaMessage.available >= 0 ? resourcequotaMessage.available : 0} {this.quotaSuffix(resourcequotaMessage.type)} {this.quotaEn(resourcequotaMessage.type)} </a>
               配额
             </div>
             <div>
