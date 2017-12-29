ddc90b1245f2222b0e152268e65a73c215b7c767
zhangxuan <zhangxuan@tenxcloud.com>
2017-12-29 11:41:31 +0800

    monitor chart add footer

--- src/components/Metrics/EchartsOption.js
+++ src/components/Metrics/EchartsOption.js
@@ -41,6 +41,12 @@ export default class EchartsOption {
       },
       axisPointer: {
         animation: false
+      },
+      position: function (pos, params, dom, rect, size) {
+        // 鼠标在左侧时 tooltip 显示到右侧，鼠标在右侧时 tooltip 显示到左侧。
+        let obj = {top: 60};
+        obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 5;
+        return obj;
       }
     }
     this.legend = {
