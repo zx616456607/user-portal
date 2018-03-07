d72ae9d3a5eadc7c22835e8a5dadf61a1f18e57e
zhangxuan <zhangxuan@tenxcloud.com>
2018-03-07 15:34:07 +0800

    [load balance] LOT-189 jira link:http://jira.tenxcloud.com/browse/LOT-189

--- src/components/AppModule/QuickCreateApp/ConfigureService/NormalSetting/IngressModal.js
+++ src/components/AppModule/QuickCreateApp/ConfigureService/NormalSetting/IngressModal.js
@@ -216,7 +216,7 @@ class IngressModal extends React.Component {
       initialValue: currentIngress ? currentIngress.port : ''
     })
   
-    const showSlider = getFieldValue('sessionSticky') && (getFieldValue('lbAlgorithm') !== 'ip_hash')
+    const showSlider = getFieldValue('sessionSticky') && (getFieldValue('lbAlgorithm') === 'round-robin')
     return (
       <Modal
         title="配置监听器"
@@ -255,7 +255,7 @@ class IngressModal extends React.Component {
             </RadioGroup>
           </FormItem>
           {
-            getFieldValue('lbAlgorithm') !== 'ip_hash' &&
+            getFieldValue('lbAlgorithm') === 'round-robin' &&
             <FormItem
               label="会话保持"
               {...formItemLayout}
