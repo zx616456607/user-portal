0ef64215ebe65be4057e4f646264ca5832e240c7
lizhen <lizhen@tenxcloud.com>
2018-04-12 15:40:10 +0800

    [LOT-402] granular resource access level. forward api request to golang api-server

--- controllers/permission.js
+++ controllers/permission.js
@@ -13,7 +13,7 @@
 'use strict'
 
 const apiFactory = require('../services/api_factory')
-const logger     = require('../utils/logger.js').getLogger("permission")
+const logger = require('../utils/logger.js').getLogger("permission")
 
 exports.list = function* () {
   const loginUser = this.session.loginUser
@@ -25,37 +25,69 @@ exports.list = function* () {
     data: result
   }
 }
-exports.get = function* (){
+exports.get = function* () {
   const id = this.params.id
   const loginUser = this.session.loginUser
   const query = this.query || {}
   var filter = query.filter
   const api = apiFactory.getPermissionApi(loginUser)
-  const result = yield api.getBy([id,'retrieve'],query)
+  const result = yield api.getBy([id, 'retrieve'], query)
   this.body = {
-  	data: result
+    data: result
   }
 }
 
-exports.listWithCount = function* (){
-	const loginUser = this.session.loginUser
+exports.listWithCount = function* () {
+  const loginUser = this.session.loginUser
   const query = this.query || {}
   var filter = query.filter
   const api = apiFactory.getPermissionApi(loginUser)
-  const result = yield api.getBy(['withCount'],query)
+  const result = yield api.getBy(['withCount'], query)
   this.body = {
-  	data: result
+    data: result
   }
 }
 
-exports.getAllDependent = function* (){
-	const id = this.params.id
-	const loginUser = this.session.loginUser
+exports.getAllDependent = function* () {
+  const id = this.params.id
+  const loginUser = this.session.loginUser
   const query = this.query || {}
   var filter = query.filter
   const api = apiFactory.getPermissionApi(loginUser)
-  const result = yield api.getBy([id,'dependent'],query)
+  const result = yield api.getBy([id, 'dependent'], query)
   this.body = {
-  	data: result
+    data: result
+  }
+}
+
+exports.listResourceOperations = function* () {
+  const api = apiFactory.getPermissionApi(this.session.loginUser)
+  const result = yield api.getBy(['resource-operations'])
+  this.body = {
+    data: result
+  }
+}
+
+exports.getAccessControlsOfRole = function* () {
+  const api = apiFactory.getPermissionApi(this.session.loginUser)
+  const result = yield api.getBy(['access-controls'], this.query)
+  this.body = {
+    data: result
+  }
+}
+
+exports.setAccessControlsForRole = function* () {
+  const api = apiFactory.getPermissionApi(this.session.loginUser)
+  const result = yield api.createBy(['access-controls'], null, this.request.body)
+  this.body = {
+    data: result
+  }
+}
+
+exports.removeAccessControlsFromRole = function* () {
+  const api = apiFactory.getPermissionApi(this.session.loginUser)
+  const result = yield api.deleteBy(['access-controls', this.params.ruleIds])
+  this.body = {
+    data: result
   }
 }
