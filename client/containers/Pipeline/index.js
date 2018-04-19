f6cdd189fa03a10adf2907f3082849e798a1b0a4
zhangpc <zhangpc@tenxcloud.com>
2018-04-19 16:50:58 +0800

    add pipeline support

---
+++ client/containers/Pipeline/index.js
@@ -0,0 +1,83 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2018 TenxCloud. All Rights Reserved.
+ */
+/**
+ * pipeline
+ *
+ * v0.1 - 2018-03-28
+ * @author zhangpc
+ */
+import React from 'react'
+import { connect } from 'react-redux'
+import { Spin } from 'antd'
+import { toQuerystring } from '../../../src/common/tools'
+import { loadApiInfo } from '../../../src/actions/open_api'
+import './style/index.less'
+
+const HEADER_HEIGHT = 60
+
+class Pipeline extends React.Component {
+  state = {
+    windowHeight: window.innerHeight,
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize)
+    const { loadApiInfo } = this.props
+    loadApiInfo()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize)
+  }
+
+  handleWindowResize = () => {
+    this.setState({
+      windowHeight: window.innerHeight,
+    })
+  }
+
+  render(){
+    const { project, token, username } = this.props
+    const { windowHeight } = this.state
+    const style = {
+      height: windowHeight - HEADER_HEIGHT,
+    }
+    if (!token) {
+      return <div className="loading">
+        <Spin size="large" />
+      </div>
+    }
+    return <div className="pipeline" style={style}>
+      <iframe id="pipeline" src={`/devops?${toQuerystring({ token, username, project })}`}  />
+    </div>
+  }
+}
+
+const mapStateToProps = state => {
+  const { space = {} } = state.entities.current
+  let onbehalfuser
+  // sys admin check user personal space
+  if (space.userName) {
+    onbehalfuser = space.userName
+    space.namespace = 'default'
+  }
+  let project
+  if (space.namespace !== 'default') {
+    project = space.namespace
+  }
+
+  const { username, token } = state.openApi.result || {}
+
+  return {
+    onbehalfuser,
+    project,
+    username,
+    token,
+  }
+}
+
+export default connect(mapStateToProps, {
+  loadApiInfo,
+})(Pipeline)
