0bd334ef7f19d4357da107b11a6343051a020625
rensiwei <vsion.ren@tenxcloud.com>
2018-05-07 15:01:24 +0800

    恢复集群管理 TabsactiveKey

--- src/components/ClusterModule/clusterTabList.js
+++ src/components/ClusterModule/clusterTabList.js
@@ -73,7 +73,7 @@ class ClusterTabList extends Component {
     const { getAllClusterNodes, clusterID } = this.props
     getAllClusterNodes(clusterID)
     this.setState({
-      TabsactiveKey: 'cluster_storage',
+      TabsactiveKey: 1,
     })
     this.loadData()
   }
