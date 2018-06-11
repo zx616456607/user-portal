18cc31ac9010f7f01b5b8cef25b3661cf04f6937
zhouhaitao <capslock.zhou@tenxcloud.com>
2018-06-11 20:50:44 +0800

    镜像仓库-公开仓库组普通用户无删除权限删除置灰

--- src/components/AppCenter/ImageCenter/ImageItem/CodeRepo.js
+++ src/components/AppCenter/ImageCenter/ImageItem/CodeRepo.js
@@ -157,14 +157,22 @@ class CodeRepo extends Component {
   }
 
   render() {
-    const { repos, projectDetail, isAdminAndHarbor, location, role, members } = this.props
+    const { repos, projectDetail, isAdminAndHarbor, location, user, members } = this.props
     const { publishModal, currentImage } = this.state
     let { isFetching, list, server, total } = repos || {}
     list = list || []
     server = server || ''
     server = server.replace('http://', '').replace('https://', '')
-    console.log(role);
-    console.log(members);
+    let currentMember = {}
+    members.every(member => {
+      if (member.username === user.userName) {
+        currentMember = member
+        return false
+      }
+      return true
+    })
+    const isAbleToDel = members.roleId === 1
+
     const columns = [
       {
         title: '镜像名',
@@ -242,7 +250,7 @@ class CodeRepo extends Component {
               <Menu.Item key="publish">
                 发布
               </Menu.Item>
-              <Menu.Item key="delete">
+              <Menu.Item key="delete" disabled={!isAbleToDel && !user.role}>
                 删除
               </Menu.Item>
             </Menu>
@@ -350,12 +358,11 @@ class CodeRepo extends Component {
 function mapStateToProps(state, props) {
   const { harbor, entities } = state
   const { loginUser } = entities
-  const role = loginUser.info.role
-
+  const user = loginUser.info
   return {
     repos: harbor.repos || {},
-    role,
-    members:harbor.members
+    user,
+    members:harbor.members.list
   }
 }
 
