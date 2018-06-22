27d559a4528ec62546ee3c25f8c3fcf78949009d
zhangpc <zhangpc@tenxcloud.com>
2018-06-22 11:49:03 +0800

    integrated backup

--- client/containers/AIDeepLearning/index.js
+++ client/containers/AIDeepLearning/index.js
@@ -11,9 +11,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Spin } from 'antd'
+import QueueAnim from 'rc-queue-anim'
 import { browserHistory } from 'react-router'
 import { toQuerystring } from '../../../src/common/tools'
 import { loadApiInfo } from '../../../src/actions/open_api'
+import SecondSider from '../../../src/components/SecondSider'
 import Title from '../../../src/components/Title'
 import './style/index.less'
 
@@ -21,9 +23,73 @@ const HEADER_HEIGHT = 60
 // replace hash when build, for clear cache
 const hash = process.env.DEVOPS_PORTAL_HASH
 
+const menus = [
+  {
+    url: '/ai-deep-learning/notebook',
+    name: 'Notebook',
+    onClick: () => {
+      try {
+        browserHistory.push('/ai-deep-learning/notebook')
+        if (window.devFlowPortalHistory) {
+          window.devFlowPortalHistory.push('/ai-deep-learning/notebook')
+        }
+      } catch (error) {
+        //
+      }
+    },
+  },
+  {
+    url: '/ai-deep-learning/large-scale-train',
+    name: '大规模训练',
+    onClick: () => {
+      try {
+        browserHistory.push('/ai-deep-learning/large-scale-train')
+        if (window.devFlowPortalHistory) {
+          window.devFlowPortalHistory.push('/ai-deep-learning/largeScaleTrain')
+        }
+      } catch (error) {
+        //
+      }
+    },
+  },
+  {
+    url: '/ai-deep-learning/data-set',
+    name: '数据集',
+    onClick: () => {
+      try {
+        browserHistory.push('/ai-deep-learning/data-set')
+        if (window.devFlowPortalHistory) {
+          window.devFlowPortalHistory.push('/ai-deep-learning/dataSet')
+        }
+      } catch (error) {
+        //
+      }
+    },
+  },
+  {
+    url: '/ai-deep-learning/model-set',
+    name: '模型集',
+    onClick: () => {
+      try {
+        browserHistory.push('/ai-deep-learning/model-set')
+        if (window.devFlowPortalHistory) {
+          window.devFlowPortalHistory.push('/ai-deep-learning/modelSet')
+        }
+      } catch (error) {
+        //
+      }
+    },
+  },
+  {
+    url: '/ai-deep-learning/ai-model-service',
+    name: 'AI 模型服务',
+  },
+]
+
 class AIDeepLearning extends React.Component {
   state = {
     windowHeight: window.innerHeight,
+    containerSiderStyle: 'normal',
   }
 
   componentWillMount() {
@@ -61,6 +127,7 @@ class AIDeepLearning extends React.Component {
     const {
       project, onbehalfuser, onbehalfuserid, token, cluster,
       username, location: { pathname, query: locationQuery },
+      children,
     } = this.props
     let redirect
     if (pathname === '/ai-deep-learning/notebook') {
@@ -81,7 +148,7 @@ class AIDeepLearning extends React.Component {
         token, username, project, onbehalfuser, onbehalfuserid, cluster, hash,
       }
     )
-    const { windowHeight } = this.state
+    const { windowHeight, containerSiderStyle } = this.state
     const style = {
       height: windowHeight - HEADER_HEIGHT,
     }
@@ -91,9 +158,39 @@ class AIDeepLearning extends React.Component {
         <Spin size="large" />
       </div>
     }
-    return <div className="pipeline" style={style}>
-      <Title title="流水线" />
-      <iframe title="流水线" id="pipeline" src={`/ai?${toQuerystring(query)}`} />
+    const scope = this
+
+    return <div id="AIDeepLearning" style={style}>
+      <QueueAnim
+        className="AIDeepLearningSiderAnimate"
+        key="AIDeepLearningSiderAnimate"
+        type="left"
+      >
+        <div
+          className={
+            containerSiderStyle === 'normal'
+              ? 'AIDeepLearningMenu CommonSecondMenu'
+              : 'hiddenMenu AIDeepLearningMenu CommonSecondMenu'
+          }
+          key="cicdSider"
+        >
+          <SecondSider menuList={menus} scope={scope} />
+        </div>
+      </QueueAnim>
+      <div
+        className={
+          containerSiderStyle === 'normal'
+            ? 'AIDeepLearningContent CommonSecondContent'
+            : 'hiddenContent AIDeepLearningContent CommonSecondContent'
+        }
+      >
+        <Title title="流水线" />
+        {
+          pathname === '/ai-deep-learning/ai-model-service'
+            ? children
+            : <iframe title="流水线" id="pipeline" src={`/ai?${toQuerystring(query)}`} />
+        }
+      </div>
     </div>
   }
 }
