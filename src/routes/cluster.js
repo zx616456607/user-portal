36fd30afc6d5559495acf132f2d7af37d0aee7b7
zhangpc <zhangpc@tenxcloud.com>
2018-06-25 19:49:33 +0800

    [LOT-1369] add project and cluster for ai, move backup to cluster

--- src/routes/cluster.js
+++ src/routes/cluster.js
@@ -10,22 +10,31 @@
  * @author GaoJian
 */
 
-const clusterRoutes = [{
-  path: ':clusterID/:cluster_name',
-  component: require('../components/ClusterModule/ClusterDetail').default,
-
-}, {
-  path: '/cluster/globalConfig',
-  component: require('../components/SettingModal/GlobalConfig').default,
-},{
-  path: '/cluster',
-  component: require('../components/ClusterModule').default,
-},{
-  path: '/cluster/cluster_autoscale',
-  component: require('../../client/containers/ClusterModule/ClusterAutoScale').default,
-},{
-  path: '/cluster/monitor',
-  component: require('../../client/containers/Monitor').default,
-}]
+const clusterRoutes = [
+  {
+    path: ':clusterID/:cluster_name',
+    component: require('../components/ClusterModule/ClusterDetail').default,
+  },
+  {
+    path: '/cluster/globalConfig',
+    component: require('../components/SettingModal/GlobalConfig').default,
+  },
+  {
+    path: '/cluster',
+    component: require('../components/ClusterModule').default,
+  },
+  {
+    path: '/cluster/cluster_autoscale',
+    component: require('../../client/containers/ClusterModule/ClusterAutoScale').default,
+  },
+  {
+    path: '/cluster/monitor',
+    component: require('../../client/containers/Monitor').default,
+  },
+  {
+    path: '/cluster/backup',
+    component: require('../../client/containers/Monitor').default,
+  },
+]
 
 export default clusterRoutes
