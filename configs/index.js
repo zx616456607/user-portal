95720af9307b5124ee4997aa0353edb393370ad6
zhangtao <zhangtao@tenxcloud.com>
2018-07-13 18:53:37 +0800

    edit configs

--- configs/index.js
+++ configs/index.js
@@ -18,7 +18,7 @@ const config = {
   running_mode: env.RUNNING_MODE || constants.ENTERPRISE_MODE, // enterprise / standard
   protocol: env.DASHBOARD_PROTOCOL || 'http',
   hostname: env.DASHBOARD_HOST || '0.0.0.0',
-  port: env.DASHBOARD_PORT || 8004,
+  port: env.DASHBOARD_PORT || 8003,
   url: env.USERPORTAL_URL || 'https://portal.tenxcloud.com', // USERPORTAL_URL env is only useful in production environments
   intl_cookie_name: 'intl_locale',
   session_key: 'tce',
