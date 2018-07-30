4f4d32e374785a033903503fddbf261040bcf818
zhouhaitao <capslock.zhou@tenxcloud.com>
2018-07-30 10:20:42 +0800

    修复backupChain.ts缩进问题

--- client/actions/backupChain.ts
+++ client/actions/backupChain.ts
@@ -41,23 +41,23 @@ export const CREATE_BACKUPCHAIN_SUCCESS = 'CREATE_BACKUPCHAIN_SUCCESS'
 export const CREATE_BACKUPCHAIN_FAILURE = 'CREATE_BACKUPCHAIN_FAILURE'
 
 const createBackupChainRequest = (id, type, name, body, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [CREATE_BACKUPCHAIN_REQUEST, CREATE_BACKUPCHAIN_SUCCESS, CREATE_BACKUPCHAIN_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${id}/daas/${type}/${name}/backups`,
-            schema: {},
-            options: {
-                method: 'POST',
-                body,
-            },
+  return {
+    [FETCH_API]: {
+        types: [CREATE_BACKUPCHAIN_REQUEST, CREATE_BACKUPCHAIN_SUCCESS, CREATE_BACKUPCHAIN_FAILURE],
+        endpoint: `${API_URL_PREFIX}/clusters/${id}/daas/${type}/${name}/backups`,
+        schema: {},
+        options: {
+           method: 'POST',
+           body,
         },
-        callback,
-    }
+    },
+    callback,
+  }
 }
 export const createBackupChain = (id: string, type: string, name: string, body: object, callback) => {
-    return dispatch => {
-        dispatch (createBackupChainRequest(id, type, name, body, callback))
-    }
+  return dispatch => {
+    dispatch (createBackupChainRequest(id, type, name, body, callback))
+  }
 }
 
 // 删除手动备份链
@@ -66,24 +66,24 @@ export const DELETE_BACKUPCHAIN_SUCCESS = 'DELETE_BACKUPCHAIN_SUCCESS'
 export const DELETE_BACKUPCHAIN_FAILURE = 'DELETE_BACKUPCHAIN_FAILURE'
 
 const deleteBackupChainRequest = (clusterId, type, clusterName, name, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [DELETE_BACKUPCHAIN_REQUEST, DELETE_BACKUPCHAIN_SUCCESS, DELETE_BACKUPCHAIN_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${clusterName}/backups/${name}`,
-            schema: {},
-            options: {
-                method: 'DELETE',
-            },
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [DELETE_BACKUPCHAIN_REQUEST, DELETE_BACKUPCHAIN_SUCCESS, DELETE_BACKUPCHAIN_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${clusterName}/backups/${name}`,
+      schema: {},
+      options: {
+        method: 'DELETE',
+      },
+    },
+    callback,
+  }
 }
 export const deleteManualBackupChain = (
-    clusterId: string, type: string, clusterName: string, name: string, callback
+  clusterId: string, type: string, clusterName: string, name: string, callback
 ) => {
-    return dispatch => {
-        dispatch (deleteBackupChainRequest(clusterId, type, clusterName, name, callback))
-    }
+  return dispatch => {
+    dispatch (deleteBackupChainRequest(clusterId, type, clusterName, name, callback))
+  }
 }
 
 // 检查是否有自动备份
@@ -92,19 +92,19 @@ export const CHECK_AUTO_BACKUP_EXIST_SUCCESS = 'CHECK_AUTO_BACKUP_EXIST_SUCCESS'
 export const CHECK_AUTO_BACKUP_EXIST_FAILURE = 'CHECK_AUTO_BACKUP_EXIST_FAILURE'
 
 const checkAutoBackupExistRequest = (clusterId, type, name, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [CHECK_AUTO_BACKUP_EXIST_REQUEST, CHECK_AUTO_BACKUP_EXIST_SUCCESS, CHECK_AUTO_BACKUP_EXIST_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
-            schema: {},
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [CHECK_AUTO_BACKUP_EXIST_REQUEST, CHECK_AUTO_BACKUP_EXIST_SUCCESS, CHECK_AUTO_BACKUP_EXIST_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
+      schema: {},
+    },
+    callback,
+  }
 }
 export const checkAutoBackupExist = (clusterId: string, type: string, name: string, callback) => {
-    return dispatch => {
-        dispatch (checkAutoBackupExistRequest(clusterId, type, name, callback))
-    }
+  return dispatch => {
+    dispatch (checkAutoBackupExistRequest(clusterId, type, name, callback))
+  }
 }
 
 // 设置自动备份
@@ -113,23 +113,23 @@ export const AUTO_BACKUP_SET_SUCCESS = 'AUTO_BACKUP_SET_SUCCESS'
 export const AUTO_BACKUP_SET_FAILURE = 'AUTO_BACKUP_SET_FAILURE'
 
 const autoBackupSetRequest = (clusterId, type, name, body, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [AUTO_BACKUP_SET_REQUEST, AUTO_BACKUP_SET_SUCCESS, AUTO_BACKUP_SET_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
-            schema: {},
-            options: {
-                method: 'POST',
-                body,
-            },
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [AUTO_BACKUP_SET_REQUEST, AUTO_BACKUP_SET_SUCCESS, AUTO_BACKUP_SET_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
+      schema: {},
+      options: {
+        method: 'POST',
+        body,
+      },
+    },
+    callback,
+  }
 }
 export const autoBackupSet = (clusterId: string, type: string, name: string, body: object, callback) => {
-    return dispatch => {
-        dispatch (autoBackupSetRequest(clusterId, type, name, body, callback))
-    }
+  return dispatch => {
+    dispatch (autoBackupSetRequest(clusterId, type, name, body, callback))
+  }
 }
 
 // 修改自动备份,同时redis的关闭自动备份也是这个，传{schedule: ''}
@@ -138,23 +138,23 @@ export const UPDATE_BACKUP_SET_SUCCESS = 'UPDATE_BACKUP_SET_SUCCESS'
 export const UPDATE_BACKUP_SET_FAILURE = 'UPDATE_BACKUP_SET_FAILURE'
 
 const updateAutoBackupSetRequest = (clusterId, type, name, body, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [UPDATE_BACKUP_SET_REQUEST, UPDATE_BACKUP_SET_SUCCESS, UPDATE_BACKUP_SET_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
-            schema: {},
-            options: {
-                method: 'PUT',
-                body,
-            },
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [UPDATE_BACKUP_SET_REQUEST, UPDATE_BACKUP_SET_SUCCESS, UPDATE_BACKUP_SET_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${name}/cronbackups`,
+      schema: {},
+      options: {
+        method: 'PUT',
+        body,
+      },
+    },
+    callback,
+  }
 }
 export const updateAutoBackupSet = (clusterId: string, type: string, name: string, body: object, callback) => {
-    return dispatch => {
-        dispatch (updateAutoBackupSetRequest(clusterId, type, name, body, callback))
-    }
+  return dispatch => {
+    dispatch (updateAutoBackupSetRequest(clusterId, type, name, body, callback))
+  }
 }
 
 // 关闭自动备份(仅限MySQL)
@@ -163,22 +163,22 @@ export const AUTO_BACKUP_DELETE_SUCCESS = 'AUTO_BACKUP_DELETE_SUCCESS'
 export const AUTO_BACKUP_DELETE_FAILURE = 'AUTO_BACKUP_DELETE_FAILURE'
 
 const autoBackupDeleteRequest = (clusterId, type, clusterName, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [AUTO_BACKUP_DELETE_REQUEST, AUTO_BACKUP_DELETE_SUCCESS, AUTO_BACKUP_DELETE_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${clusterName}/cronbackups`,
-            schema: {},
-            options: {
-                method: 'DELETE',
-            },
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [AUTO_BACKUP_DELETE_REQUEST, AUTO_BACKUP_DELETE_SUCCESS, AUTO_BACKUP_DELETE_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${clusterName}/cronbackups`,
+      schema: {},
+      options: {
+        method: 'DELETE',
+      },
+    },
+    callback,
+  }
 }
 export const autoBackupDetele = (clusterId: string, type: string, clusterName: string, callback) => {
-    return dispatch => {
-        dispatch (autoBackupDeleteRequest(clusterId, type, clusterName, callback))
-    }
+  return dispatch => {
+    dispatch (autoBackupDeleteRequest(clusterId, type, clusterName, callback))
+  }
 }
 
 // 回滚
@@ -187,21 +187,21 @@ export const ROLLBACK_SUCCESS = 'ROLLBACK_SUCCESS'
 export const ROLLBACK_FAILURE = 'ROLLBACK_FAILURE'
 
 const rollbackRequest = (clusterId, type, pointName, body, callback) => {
-    return {
-        [FETCH_API]: {
-            types: [ROLLBACK_REQUEST, ROLLBACK_SUCCESS, ROLLBACK_FAILURE],
-            endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${pointName}/restores`,
-            schema: {},
-            options: {
-                method: 'POST',
-                body,
-            },
-        },
-        callback,
-    }
+  return {
+    [FETCH_API]: {
+      types: [ROLLBACK_REQUEST, ROLLBACK_SUCCESS, ROLLBACK_FAILURE],
+      endpoint: `${API_URL_PREFIX}/clusters/${clusterId}/daas/${type}/${pointName}/restores`,
+      schema: {},
+      options: {
+        method: 'POST',
+        body,
+      },
+    },
+    callback,
+  }
 }
 export const postRollback = (clusterId: string, type: string, pointName: string, body: object, callback) => {
-    return dispatch => {
-        dispatch (rollbackRequest(clusterId, type, pointName, body, callback))
-    }
+  return dispatch => {
+    dispatch (rollbackRequest(clusterId, type, pointName, body, callback))
+  }
 }
