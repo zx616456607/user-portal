768503a5f06806a1587225818e26ffae73c14aeb
zhouhaitao <capslock.zhou@tenxcloud.com>
2018-08-03 16:01:37 +0800

    去掉无用代码

--- src/components/SettingModal/GlobalConfig/index.js
+++ src/components/SettingModal/GlobalConfig/index.js
@@ -1784,7 +1784,6 @@ class GlobalConfig extends Component {
   }
 
   componentWillMount() {
-    const compose = (...args) => args.reduce((pre, d) => num => pre(d(num)))
     this.props.loadGlobalConfig(this.props.cluster.clusterID, {
       success: {
         func: (result) => {
