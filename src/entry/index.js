b5cd8c9e0a1f9c94b1a68425f2aef71091b731fe
zhangtao <zhangtao@tenxcloud.com>
2018-08-15 15:29:44 +0800

    升级react-hot-loader至4.x版本

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,3 +1,12 @@
+/*
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2018 TenxCloud. All Rights Reserved.
+ * ----
+ * index.js page
+ *
+ * @author zhangtao
+ * @date Wednesday August 15th 2018
+ */
 /**
  * Licensed Materials - Property of tenxcloud.com
  * (C) Copyright 2016 TenxCloud. All Rights Reserved.
@@ -8,8 +17,24 @@
  * @author Zhangpc
  */
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./index.prod')
-} else {
-  module.exports = require('./index.dev')
-}
+import '@babel/polyfill'
+import '../common/lib'
+import React from 'react'
+import { render } from 'react-dom'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import Root from '../containers/Root'
+import configureStore from '../store/configureStore'
+import Notification from '../components/Notification'
+import '@tenx-ui/icon/assets/index.less'
+
+const notification  = new Notification()
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+notification.init(store)
+
+render(
+  <Root store={store} history={history} />,
+  document.getElementById('root')
+)
+
