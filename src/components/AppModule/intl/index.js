e7c059c4f522a710a8bd811cc4778d82ded4dba4
zhangtao <zhangtao@tenxcloud.com>
2018-08-23 18:45:50 +0800

    完成servicelist国际化文本抽取

---
+++ src/components/AppModule/intl/index.js
@@ -0,0 +1,47 @@
+import { defineMessages } from 'react-intl'
+import AllServiceListIntl from './AllServiceListIntl'
+
+export { AllServiceListIntl }
+
+export default  defineMessages({
+  start: {
+    id: 'ServiceCommon.start',
+    defaultMessage: '启动'
+  },
+  stop: {
+    id: 'ServiceCommon.stop',
+    defaultMessage: '停止'
+  },
+  refresh: {
+    id: 'ServiceCommon.refresh',
+    defaultMessage: '刷新'
+  },
+  delete: {
+    id: 'ServiceCommon.delete',
+    defaultMessage: '删除'
+  },
+  reboot: {
+    id: 'ServiceCommon.reboot',
+    defaultMessage: '重启'
+  },
+  common: {
+    id: 'ServiceCommon.common',
+    defaultMessage: '共'
+  },
+  page: {
+    id: 'ServiceCommon.page',
+    defaultMessage: '条'
+  },
+  moreOperation: {
+    id: 'ServiceCommon.moreOperation',
+    defaultMessage: '更多操作'
+  },
+  status: {
+    id: 'ServiceCommon.status',
+    defaultMessage: '状态'
+  },
+  operation: {
+    id: 'ServiceCommon.operation',
+    defaultMessage: '操作'
+  },
+})
