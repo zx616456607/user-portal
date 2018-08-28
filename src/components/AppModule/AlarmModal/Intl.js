7e327dede87488d7bbb61df46e5f01dd52ee467c
songsz <link.song@tenxcloud.com>
2018-08-28 17:04:22 +0800

    [LOT-2114]AppModual/AlarmModal/index.js ok

---
+++ src/components/AppModule/AlarmModal/Intl.js
@@ -0,0 +1,65 @@
+/**
+ * Licensed Materials - Property of tenxcloud.com
+ * (C) Copyright 2018 TenxCloud. All Rights Reserved.
+*/
+
+/**
+ *
+ * define Intl Msg for ClusterModule
+ *
+ * @author Songsz
+ * @date 2018-08-23
+ *
+*/
+
+import { defineIntlMessages } from "../../../common/tools"
+import React from "react";
+import {Button} from "antd";
+// data for index, clusterTabList
+export const mapData = {
+  prefix: 'AlarmModal',
+  data: {
+    plsInputName: '请输入名称',
+    plsInput340: '请输入3~40位字符',
+    plsInputCnEnNum: '请输入中文、英文字母或数字开头，中间可下划线、连接符',
+    stgNameRepeat: '策略名称重复',
+    nameIllegal: '名称包含非法字符',
+    plsSlcType: '请选择类型',
+    plsSlcNode: '请选择节点',
+    plsSlcApp: '请选择应用',
+    plsSlcServer: '请选择服务',
+    node: '节点',
+    server: '服务',
+    slcMonitorCycle: '请选择监控周期',
+    name: '名称',
+    type: '类型',
+    monitorCycle: '监控周期',
+    min5: '5分钟',
+    min30: '30分钟',
+    hour1: '1小时',
+    monitorObj: '监控对象',
+    monitorServer: '监控服务',
+    atLeastOneRule: '至少得有一项规则',
+    a: '个',
+    plsSlcOperator: '请选择运算符',
+    alarmSettingRepeat: '告警设置填写重复',
+    plsInputNum: '请填写数值',
+    valueBig1: '此数值需大于1',
+    cpuUseRate: 'CPU利用率',
+    memoryUseRate: '内存使用率',
+    memoryUse: '内存使用',
+    uploadFlow: '上传流量',
+    downloadFlow: '下载流量',
+    diskUseRate: '磁盘利用率',
+    tcpListenConnectNum: 'tcp listen连接数',
+    tcpEsConnectNum: 'tcp established连接数',
+    tcpCloseConnectNum: 'tcp close_wait连接数',
+    tcpTimeConnectNum: 'tcp time_wait连接数',
+    cpuRateFormula: '所有容器实例占用CPU总和/CPU资源总量',
+    memoryRateFormula: '所有容器实例占用内存总和/容器实例数量',
+    upStep: '上一步',
+    nextStep: '下一步',
+  }
+}
+
+export default defineIntlMessages(mapData)
