4bb6182af9a41cc54800d31ddd4d7156e65510c3
zhangtao <zhangtao@tenxcloud.com>
2018-09-07 14:37:35 +0800

    LOT-2063 优化不能修改的应用配置项目 前端改造完成

--- src/components/AppModule/AppServiceDetail/AppServiceAssistSetting.js
+++ src/components/AppModule/AppServiceDetail/AppServiceAssistSetting.js
@@ -9,17 +9,20 @@
  * @author Zhangpc
  */
 import React from 'react'
-import { Card, Spin, Form, Input, Checkbox, Radio, Icon,Tooltip } from 'antd'
+import { Card, Spin, Form, Input, Checkbox, Radio, Icon,Tooltip, Button } from 'antd'
 import './style/AppServiceAssistSetting.less'
 import { formatDate } from '../../../common/tools'
 import { TENX_LOCAL_TIME_VOLUME } from '../../../../constants'
+import cloneDeep from 'lodash/cloneDeep'
 
 import ServiceCommonIntl, { AppServiceDetailIntl, AllServiceListIntl } from '../ServiceIntl'
 import { injectIntl,  } from 'react-intl'
+import isEqual from 'lodash/isEqual'
 
 const createForm = Form.create
 const FormItem = Form.Item
 const RadioGroup = Radio.Group
+
 
 let AppServiceAssistSetting = React.createClass({
   propTypes: {
@@ -27,18 +30,26 @@ let AppServiceAssistSetting = React.createClass({
   },
 
   getInitialState() {
-    return {}
+    return {
+      buttonLock: true,
+      buttonLoding: false,
+    }
   },
-
-  renderCMD(container) {
+  openButtonLock(e) {
+    e.preventDefault()
+    console.log('open')
+    this.setState({ buttonLock: false })
+  },
+  renderCMD(container, getFieldProps, openButtonLock) {
     const { formatMessage } = this.props.intl
-    const { command } = container
+    let { command } = container
     if (!command || command.length == 0) {
-      return (
-        <div className="empty">
-          {formatMessage(AppServiceDetailIntl.mirrorDefault)}
-        </div>
-      )
+      // return (
+      //   <div className="empty">
+      //     {formatMessage(AppServiceDetailIntl.mirrorDefault)}
+      //   </div>
+      // )
+      command = [null] //null 代表默认点
     }
     return command.map((cmd, index) => (
       <FormItem
@@ -49,14 +60,15 @@ let AppServiceAssistSetting = React.createClass({
         <Input
           className="entryInput"
           size="large"
-          value={cmd}
-          disabled={true} />
+          {...getFieldProps(`cmd_${index}`, { initialValue: cmd ||  formatMessage(AppServiceDetailIntl.mirrorDefault),
+            onChange: openButtonLock})}
+          disabled={false} />
           </Tooltip>
       </FormItem>
     ))
   },
 
-  renderArgs(container) {
+  renderArgs(container, getFieldProps, openButtonLock) {
     const { formatMessage } = this.props.intl
     const { args } = container
     if (!args || args.length == 0) {
@@ -75,21 +87,23 @@ let AppServiceAssistSetting = React.createClass({
         <Input
           className="entryInput"
           size="large"
-          value={arg}
-          disabled={true} />
+          {...getFieldProps(`arg_${index}`, { initialValue: arg,  onChange: openButtonLock })}
+          disabled={false}
+          />
           </Tooltip>
       </FormItem>
     ))
   },
 
-  renderImagePullPolicy(container) {
+  renderImagePullPolicy(container, getFieldProps, openButtonLock) {
     const { formatMessage } = this.props.intl
     const { imagePullPolicy } = container
     return (
-      <RadioGroup value={imagePullPolicy}>
-        <Radio disabled={true} key="IfNotPresent" value="IfNotPresent">{formatMessage(AppServiceDetailIntl.priorityUseLocalMirror)}</Radio>
+      <RadioGroup {...getFieldProps( 'imagePullPolicy', { initialValue: imagePullPolicy,  onChange: openButtonLock } )}
+    >
+        <Radio disabled={false} key="IfNotPresent" value="IfNotPresent">{formatMessage(AppServiceDetailIntl.priorityUseLocalMirror)}</Radio>
         {/*<Radio disabled={true} key="Never" value="Never">始终使用本地镜像</Radio>*/}
-        <Radio disabled={true} key="Always" value="Always">{formatMessage(AppServiceDetailIntl.alwaysUseCloudMirror)}</Radio>
+        <Radio disabled={false} key="Always" value="Always">{formatMessage(AppServiceDetailIntl.alwaysUseCloudMirror)}</Radio>
       </RadioGroup>
     )
   },
@@ -110,11 +124,17 @@ let AppServiceAssistSetting = React.createClass({
     })
     return checked
   },
-
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('收到表单值：', this.props.form.getFieldsValue());
+    this.setState({ buttonLock: true, buttonLoding: true })
+  },
   render() {
     const { isFetching, serviceDetail, form } = this.props
     const { formatMessage } = this.props.intl
     const { getFieldProps } = form
+    const { buttonLock, buttonLoding } = this.state
+    const openButtonLock = this.openButtonLock
     if (isFetching || !serviceDetail.metadata) {
       return (
         <div className="loadingBox">
@@ -124,6 +144,7 @@ let AppServiceAssistSetting = React.createClass({
     }
     const { spec } = serviceDetail
     const { containers } = spec.template.spec
+
     if (!containers || containers.length == 0) {
       return (
         <div className="loadingBox">
@@ -135,17 +156,21 @@ let AppServiceAssistSetting = React.createClass({
       <Card id="AppServiceAssistSetting">
         <div className="info commonBox">
           <span className="titleSpan">{formatMessage(AppServiceDetailIntl.assistConfig)}</span>
+          <Form onSubmit={this.handleSubmit}>
+          <div className="submitButton">
+            <Button disabled={buttonLock} type="primary" htmlType="submit" loading={buttonLoding} >应用修改</Button>
+          </div>
           <div className="assitBox">
             <div>
               <div className="inputBox">
                 <span className="commonSpan">{formatMessage(AppServiceDetailIntl.intoPoint)}</span>
-                {this.renderCMD(containers[0])}
+                {this.renderCMD(containers[0], getFieldProps, openButtonLock)}
                 <div style={{ clear: "both" }}></div>
               </div>
               <div className="inputBox">
                 <span className="commonSpan">{formatMessage(AppServiceDetailIntl.starCommand)}</span>
                 <div className="selectBox" style={{height: 'auto'}}>
-                  {this.renderArgs(containers[0])}
+                  {this.renderArgs(containers[0], getFieldProps, openButtonLock)}
                 </div>
 
                 <div style={{ clear: "both" }}></div>
@@ -153,14 +178,16 @@ let AppServiceAssistSetting = React.createClass({
               <div className="inputBox">
                 <span className="commonSpan">{formatMessage(AppServiceDetailIntl.redeploy)}</span>
                 <div className="selectBox">
-                  {this.renderImagePullPolicy(containers[0])}
+                  {this.renderImagePullPolicy(containers[0], getFieldProps, openButtonLock)}
                 </div>
                 <div style={{ clear: "both" }}></div>
               </div>
               <div className="inputBox">
                 <span className="commonSpan">{formatMessage(AppServiceDetailIntl.timeZoneSet)}</span>
                 <div className="checkBox">
-                  <Checkbox disabled={true} checked={this.getLocaltime(containers[0])} />
+                  <Checkbox disabled={false} onChange={this.openButtonLock}
+                  {...getFieldProps('timeSet', { initialValue: this.getLocaltime(containers[0]), valuePropName: 'checked',
+                  onChange: openButtonLock})} />
                   <span className="checkTitle">{formatMessage(AppServiceDetailIntl.useHostNodeTimeZone)}</span><br />
                   <span className="tooltip">{formatMessage(AppServiceDetailIntl.choicePromiseContainerAndHostNode)}</span>
                 </div>
@@ -168,6 +195,7 @@ let AppServiceAssistSetting = React.createClass({
               </div>
             </div>
           </div>
+          </Form>
         </div>
       </Card>
     )
