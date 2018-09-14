a2808ebaac6cc25af08e57a6811f6a181ef131bd
zhangtao <zhangtao@tenxcloud.com>
2018-09-14 17:43:45 +0800

    修复已知bug

--- src/components/AppModule/AppServiceDetail/FilterTabs.js
+++ src/components/AppModule/AppServiceDetail/FilterTabs.js
@@ -18,12 +18,19 @@ export default function TenxTabFactory(url, filterKey) {
     render() {
       const appCenterChoiceShowRegx = url
       const appCenterChoiceHidden = appCenterChoiceShowRegx.test(window.location.pathname)
+      const children = this.props.children
+      // react 包含的子组件的形式有很多种
+      /**
+      常规方式 [{},{},{}] 每个对象代表一个组件
+      也有可能, 会出现 [{}, [{}, {}], {}] 这种方式react也是可以渲染的, 为了兼容这种情况, 添加下面这行代码
+      */
+      const spreadChildren = children.concat(...children.filter((item) => Array.isArray(item)) )
       const newChildren = appCenterChoiceHidden ?
-      this.props.children.filter(({key}) => filterKey.includes(key)).map(item => {
+      spreadChildren.filter(({key}) => filterKey.includes(key)).map(item => {
         item.props.children.props.appCenterChoiceHidden = true // 对子组件注入props
         return item
       })
-      :  this.props.children
+      :  children
       return <div>
         <Tabs {...this.props}>
         {newChildren}
