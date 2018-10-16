7945300cc2fec024420825cb1df824a12c39a983
zhangtao <zhangtao@tenxcloud.com>
2018-10-16 17:42:53 +0800

    去掉文件头部多余注释

--- client/containers/Monitor/index.js
+++ client/containers/Monitor/index.js
@@ -1,12 +1,3 @@
-/*
- * Licensed Materials - Property of tenxcloud.com
- * (C) Copyright 2018 TenxCloud. All Rights Reserved.
- * ----
- * index.js page
- *
- * @author zhangtao
- * @date Tuesday October 16th 2018
- */
 /**
  * Licensed Materials - Property of tenxcloud.com
  * (C) Copyright 2018 TenxCloud. All Rights Reserved.
