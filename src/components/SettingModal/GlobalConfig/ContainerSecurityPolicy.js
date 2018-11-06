1552a64367aaf5616da1b05315d6b5cc7bbdffc7
zhangtao <zhangtao@tenxcloud.com>
2018-11-06 18:04:57 +0800

    '租户管理 项目管理增加容器安全组tab ui完成'

--- src/components/SettingModal/GlobalConfig/ContainerSecurityPolicy.js
+++ src/components/SettingModal/GlobalConfig/ContainerSecurityPolicy.js
@@ -8,17 +8,12 @@
  * @date Tuesday November 6th 2018
  */
 import * as React from 'react'
-import { Card, Table, Dropdown, Menu, Modal } from 'antd'
+import { Table, Modal,  Row, Col, Button } from 'antd'
 import './style/ContainerSecurityPolicy.less'
 import {toQuerystring} from '../../../../src/common/tools'
 import { browserHistory } from 'react-router'
 
 const getColumns = (self) =>  {
-  const menu = (
-    <Menu onClick={self.showDelete}>
-      <Menu.Item key="1">删除</Menu.Item>
-    </Menu>
-  );
   const columns = [{
     title: '策略名称',
     dataIndex: 'policy',
@@ -37,14 +32,12 @@ const getColumns = (self) =>  {
     render: () => {
       const query = toQuerystring({edit: true, type: 'containerSecurityPolicy', name: 'null' })
       return (
-        <Dropdown.Button
-          onClick={() => {
-            browserHistory.push(`/app-stack/createWorkLoad/?${query}`)
-          }}
-          overlay={menu} type="ghost"
-        >
+        <div className="buttons">
+        <Button type="primary" onClick={() => browserHistory.push(`/app-stack/createWorkLoad/?${query}`)}>
           查看/编辑Yaml
-        </Dropdown.Button>
+        </Button>
+        <Button className="delete" onClick={self.showDelete}>删除</Button>
+        </div>
       )
     }
   }];
@@ -76,9 +69,19 @@ export default class ContainerSecurityPolicy extends React.Component {
       <div className="containerSecurityPolicy">
         <div className='title'>容器安全策略</div>
         <div className="content">
-        <Card>
-          <Table columns={getColumns(self)} dataSource={dataSource} pagination={false}/>
-        </Card>
+        <Row>
+          <Col span={4}>
+            <img></img>
+          </Col>
+          <Col span={20}>
+            <Button icon="plus" type="primary"
+            onClick={ () => browserHistory.push(`/app-stack/createWorkLoad/`) }
+            >
+              添加 PSP 策略
+            </Button>
+            <Table className="Table" columns={getColumns(self)} dataSource={dataSource} pagination={false}/>
+        </Col>
+        </Row>
         <Modal title="确认删除"
           visible={this.state.showDelete}
           onOk={() => {}}
