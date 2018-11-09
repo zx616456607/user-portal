0402bd05123f6be32895ab7b69cc91e15d0ae964
yubiao <yubiao@tenxcloud.com>
2018-11-09 16:59:21 +0800

    [KK-463] 接入EasyStack websso

---
+++ 3rd_account/openstack/redirect.js
@@ -0,0 +1,29 @@
+
+const openstackConfig = require('../../configs/3rd_account/openstack')
+const common = require('./common')
+
+
+exports.redirect = common.wrapHandler(function*() {
+  token = this.session.loginUser.openstack.withoutProject.keystoneToken
+  this.type = "html"
+  this.body = `<html>
+  <body>
+  </body>
+  <script>
+    window.onload = function() {
+      let form = document.createElement("form")
+      form.method="post"
+      form.enctype="application/json"
+      form.action="${openstackConfig.webssoURL}"
+      let input = document.createElement("input")
+      input.type="hidden"
+      input.name="token"
+      input.value = "${token}"
+      form.appendChild(input)
+      document.body.appendChild(form)
+      form.submit()
+    }
+  </script>
+  </html> 
+  `
+}, true)
--- configs/3rd_account/openstack.js
+++ configs/3rd_account/openstack.js
@@ -3,14 +3,15 @@ const env = process.env
 
 module.exports = {
   username: env.OPENSTACK_USER || "admin",
-  password: env.OPENSTACK_PASSWORD || "teamsun",
+  password: env.OPENSTACK_PASSWORD || "Admin@ES20!8",
   project: env.OPENSTACK_PROJECT || "admin",
-  host: env.OPENSTACK_HOST || '192.168.2.22',
+  host: env.OPENSTACK_HOST || 'keystone.openstack.svc.cluster.local',
   vmPort: env.OPENSTACK_VM_PROT || '8774',
   protocol: env.OPENSTACK_PROTOCOL || 'http',
-  authPort: env.OPENSTACK_AUTH_PORT || '5000',
+  authPort: env.OPENSTACK_AUTH_PORT || '80',
   metersPort: env.OPENSTACK_METERS_PORT || '8777',
   imagePort: env.OPENSTACK_IMAGE_PORT || '9292',
   networkPort: env.OPENSTACK_NETWORK_PORT || '9696',
-  volumePort: env.OPENSTACK_VOLUME_PORT || '8776'
+  volumePort: env.OPENSTACK_VOLUME_PORT || '8776',
+  webssoURL: env.ES_WEBSSO_URL || "https://101.124.23.97/auth/websso/"
 }
