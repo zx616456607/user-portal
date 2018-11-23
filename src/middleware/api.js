6171295af11201fbb2263303a97a636152fff454
zhangtao <zhangtao@tenxcloud.com>
2018-11-23 16:29:38 +0800

    [KK-839] 服务网格，组件绑定服务后，产生的相关服务标签，目前可以被删掉和修改，请禁掉

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -19,6 +19,7 @@ const SKIP_CAMELIZE_KEYS_ENDPOINTS = [
   /^\/clusters\/[\-\w]+\/secrets/,
   /^\/devops\/ci\-flows\/[\-\w]+\/getBuildLogs/,
   /^\/clusters\/[\-\w]+\/services\/[\-\w]+\/metrics/,
+  /^\/clusters\/[\-\w]+\/services/,
   /^\/clusters\/[\-\w]+\/metric\/nexport/,
   /^\/cluster-nodes\/[\-\w]+\/nodes\/resource-consumption/,
   /^\/permission\/access\-controls\/overview/,
