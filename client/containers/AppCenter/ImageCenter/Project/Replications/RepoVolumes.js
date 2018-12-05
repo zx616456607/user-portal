a538cbd32790dac0014668f77e2a312d89fed1bf
lvjunfeng <jfeng.lv@tenxcloud.com>
2018-12-05 16:44:50 +0800

    [KK-1453]普通成员登录，仓库组中存储容量显示NaN

--- client/containers/AppCenter/ImageCenter/Project/Replications/RepoVolumes.js
+++ client/containers/AppCenter/ImageCenter/Project/Replications/RepoVolumes.js
@@ -12,14 +12,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Progress, Row, Col } from 'antd'
+import Notification from '../../../../../../src/components/Notification'
 import * as harborAction from '../../../../../../src/actions/harbor'
 import { DEFAULT_REGISTRY } from '../../../../../../src/constants'
 import './style/RepoVolumes.less'
+
+const notification = new Notification()
 class RepoVolumes extends React.Component {
 
   componentDidMount() {
     const { getSysteminfoVolumes } = this.props
-    getSysteminfoVolumes(DEFAULT_REGISTRY)
+    getSysteminfoVolumes(DEFAULT_REGISTRY, {
+      failed: {
+        func: error => {
+          if (error.statusCode === 403) {
+            return null
+          }
+          notification.warn('获取存储容量失败')
+        },
+      },
+    })
   }
 
   render() {
@@ -27,6 +39,10 @@ class RepoVolumes extends React.Component {
     const all = Math.round(total / 1024 / 1024 / 1024)
     const used = Math.round((total - free) / 1024 / 1024 / 1024)
     const percent = (used / all * 100).toFixed(2)
+    // 普通用户无权限，仓库组中不显示
+    if (!total && !free) {
+      return null
+    }
     return <Row className="repoVolumesRow">
       <Col style={{ padding: 0 }}>
         &nbsp;存储容量( {used} G / {all} G )&nbsp;
