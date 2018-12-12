c0bf1f8e53ce0c25a4f7e78f67f285c35416804e
rensiwei <vsion.ren@tenxcloud.com>
2018-12-12 16:16:19 +0800

    [KK-1598]

--- src/components/AppModule/VMWrap/VMList.js
+++ src/components/AppModule/VMWrap/VMList.js
@@ -474,7 +474,6 @@ class VMList extends React.Component {
       createTomcat(values, {
         success: {
           func: res => {
-            console.log(res)
             if (res.statusCode === 200) {
               notification.success(`新建 ${values.name} 成功`)
               this.setState({
@@ -486,13 +485,13 @@ class VMList extends React.Component {
           isAsync: true,
         },
         failed: {
-          func: err => {
-            if (err.statusCode === 500) {
-              notification.warn('端口号重复')
-            } else {
-              notification.warn('新建 Tomcat 失败')
-            }
-          }
+          func: () => {
+            // if (err.statusCode === 500) {
+            //   notification.warn('端口号重复')
+            // } else {
+            notification.warn('新建 Tomcat 失败')
+            // }
+          },
         },
         finally: {
           func: () => {
