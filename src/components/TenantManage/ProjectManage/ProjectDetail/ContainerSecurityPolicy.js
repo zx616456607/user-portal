675fbe6d6cb0680afd7ca6cd8f3d72c0c75f2953
zhangtao <zhangtao@tenxcloud.com>
2018-12-27 19:28:52 +0800

    [KK-1955]【PSP策略】集群只有一条privileged容器安全策略，项目内未开启

--- src/components/TenantManage/ProjectManage/ProjectDetail/ContainerSecurityPolicy.js
+++ src/components/TenantManage/ProjectManage/ProjectDetail/ContainerSecurityPolicy.js
@@ -14,16 +14,31 @@ let yaml = require('js-yaml')
 const userAReg = /^users\/annotations$/
 const getColumns = (self) =>  {
   const columns = [{
-    title: '策略名称',
-    dataIndex: 'policy',
-    key: 'policy',
+    title: '集群名称',
+    dataIndex: 'clusterName',
+    key: 'clusterName',
     width: 300
   },{
-    title: '状态',
-    dataIndex: 'status',
-    key: 'status',
+    title: '绑定策略',
+    dataIndex: 'bindPolicy',
+    key: 'bindPolicy',
     width: 300,
-    render: (status) => <Status status={status}/>,
+    render: (_, record) => {
+      const currentPsp = self.state.currentOpenPsp[record.tableIndex]
+
+      return <Select 
+        placeholder='请选择集群'
+        value={currentPsp}
+        style={{ width: 120 }} onChange={(value) => { self.handleChange(value, record.tableIndex) }}
+        notFoundContent={'暂无集群'}
+        disabled={!self.state.edit}
+      >
+        { record.policyArray.map(({ policy }) => {
+          return <Option value={policy}>{policy}</Option>
+        })
+        }
+      </Select>
+    }
   }, {
     title: <span>
     <span style={{ padding: '0 8px' }}>注释</span>
@@ -34,7 +49,9 @@ const getColumns = (self) =>  {
     dataIndex: 'annotation',
     key: 'annotation',
     width: 300,
-    render: (annotation = []) => {
+    render: (data, record) => {
+      const currentPolicy = record.policyArray.filter(({ policy }) => policy === self.state.currentOpenPsp[record.tableIndex] )
+      const annotation = currentPolicy[0].annotation || {}
       const userAnnotation =  Object.entries(annotation)
       .filter(([key]) => userAReg.test(key))
       if (userAnnotation.length === 0) return <span>-</span>
@@ -52,28 +69,30 @@ const getColumns = (self) =>  {
       <span className="annotation">查看注释</span>
     </Popover>
     }
-  }, {
-    title: '操作',
-    dataIndex: 'operation',
-    key: 'operation',
-    width: 300,
-    render: (_, record) => {
-      return (
-        <div className="buttons">
-        <Button type="primary" onClick={() =>
-          self.setState( { currentPSP: record.policy },
-            () => self.setState({showYaml: true})) }
-            disabled={self.props.roleNum !== 2}
-            >
-          查看Yaml
-        </Button>
-        <Button className="delete" disabled={self.props.roleNum !== 2} onClick={() => self.showDelete(record)}>
-        {record.status === 'opening' ? '关闭' : '开启'}
-        </Button>
-        </div>
-      )
-    }
-  }];
+  }, 
+  // {
+  //   title: '操作',
+  //   dataIndex: 'operation',
+  //   key: 'operation',
+  //   width: 300,
+  //   render: (_, record) => {
+  //     return (
+  //       <div className="buttons">
+  //       <Button type="primary" onClick={() =>
+  //         self.setState( { currentPSP: record.policy },
+  //           () => self.setState({showYaml: true})) }
+  //           disabled={self.props.roleNum !== 2}
+  //           >
+  //         查看Yaml
+  //       </Button>
+  //       {/* <Button className="delete" disabled={self.props.roleNum !== 2} onClick={() => self.showDelete(record)}>
+  //       {record.status === 'opening' ? '关闭' : '开启'}
+  //       </Button> */}
+  //       </div>
+  //     )
+  //   }
+  // }
+];
   return columns
 }
 
@@ -89,23 +108,41 @@ class ContainerSecurityPolicy extends React.Component {
     showDelete: false,
     currentRecord: {},
     operationLoading: false,
+    edit: false,
+    currentOpenPsp: [],
   }
-  async componentDidMount() {
+  componentDidMount() {
+    this.loadAll()
+  }
+  loadAll = async () => {
     const { namespace } = this.props.projectDetail
     const res = await this.props.getProjectVisibleClusters(namespace)
     const clustersArray = (getDeepValue(res, ['response', 'result', 'data', 'clusters']) || [])
     .map(({ clusterName, clusterID }) => ({clusterName, clusterID}))
-    this.setState({ clustersArray })
-    if ((clustersArray[0] || {}).clusterID !== undefined) {
-      this.setState({ currentValue: (clustersArray[0] || {}).clusterID })
-    }
-    await this.listPSP()
+    // this.setState({ clustersArray })
+    // if ((clustersArray[0] || {}).clusterID !== undefined) {
+    //   this.setState({ currentValue: (clustersArray[0] || {}).clusterID })
+    // }
+    const asyncFunc = clustersArray.map(({ clusterID }) => {
+      return this.listPSP.call(this, clusterID)
+    })
+    const policyArray = await Promise.all(asyncFunc)
+    const currentOpenPsp = policyArray.map((ipolicyArray) => {
+      const iPolicy = ipolicyArray.filter(({ status }) => status === 'opening')
+      if (iPolicy.length === 0) { return undefined }
+      return iPolicy[0].policy
+    })
+    this.setState({ currentOpenPsp })
+    const dataList = clustersArray.map((node, index) => {
+      return Object.assign({}, { policyArray: policyArray[index] }, node, { tableIndex: index } )
+    })
+    this.setState({ dataList })
   }
-  listPSP = async () => {
+  listPSP = async (currentValue) => {
     const { namespace } = this.props.projectDetail
-    if (this.state.currentValue === undefined) { return }
-    const res = await this.props.listPSP(this.state.currentValue)
-    const resProject = await this.props.listProjectPSPDetail(this.state.currentValue, namespace)
+    // if (this.state.currentValue === undefined) { return }
+    const res = await this.props.listPSP(currentValue)
+    const resProject = await this.props.listProjectPSPDetail(currentValue, namespace)
     const { result: { data = [] } = {} } = res.response
     const { result: { data:openPSP = [] } } = resProject.response
     const dataList = data.map(({ metadata: { name, annotations } = {} }) =>
@@ -113,11 +150,14 @@ class ContainerSecurityPolicy extends React.Component {
        policy: name,
        annotation: annotations,
        status: (openPSP || []).includes(name) ? "opening": "closed" }))
-    this.setState({ dataList })
+    return dataList
   }
-  handleChange =async (value) => {
-    await this.setState({ currentValue: value, dataList: undefined})
-    await this.listPSP()
+  handleChange =async (value, index) => {
+    const newcurrentOpenPsp = [...this.state.currentOpenPsp]
+    newcurrentOpenPsp[index] = value
+    this.setState({
+      currentOpenPsp: newcurrentOpenPsp
+    })
   }
   showDelete = (record) => {
     this.setState({ currentRecord: record }, () => {
@@ -125,16 +165,18 @@ class ContainerSecurityPolicy extends React.Component {
     })
   }
   openOrDelete = async () => {
-    const { namespace } = this.props.projectDetail
-    this.setState({ operationLoading: true })
-    if (this.state.currentRecord.status === 'opening') {
-      await this.props.stopPSPProject(this.state.currentValue, this.state.currentRecord.policy, namespace)
-    }
-    if (this.state.currentRecord.status === 'closed') {
-      await this.props.startPodProject(this.state.currentValue, this.state.currentRecord.policy, namespace)
-    }
-    this.setState({ operationLoading: false, showDelete: false })
-    this.listPSP()
+    //TODO: 需要后端改接口
+    
+    // const { namespace } = this.props.projectDetail
+    // this.setState({ operationLoading: true })
+    // if (this.state.currentRecord.status === 'opening') {
+    //   await this.props.stopPSPProject(this.state.currentValue, this.state.currentRecord.policy, namespace)
+    // }
+    // if (this.state.currentRecord.status === 'closed') {
+    //   await this.props.startPodProject(this.state.currentValue, this.state.currentRecord.policy, namespace)
+    // }
+    // this.setState({ operationLoading: false, showDelete: false })
+    // this.listPSP()
   }
   render() {
     const self = this
@@ -145,7 +187,7 @@ class ContainerSecurityPolicy extends React.Component {
           <span style={{ marginLeft: '8px' }}>开启 PSP 策略后，系统会将策略绑定到当前项目</span>
         </div>
 
-        <Select
+        {/* <Select
           placeholder='请选择集群'
           value={this.state.currentValue}
           style={{ width: 120 }} onChange={this.handleChange}
@@ -155,13 +197,23 @@ class ContainerSecurityPolicy extends React.Component {
               <Option value={clusterID}>{clusterName}</Option>
             )
           }
-        </Select>
+        </Select> */}
         <Table
           columns={getColumns(self)}
           dataSource={this.state.dataList}
           pagination={false}
           loading={this.state.dataList === undefined}
           />
+          <div className="editOperation">
+            {
+              this.state.edit ? 
+              <div>
+                <Button onClick={() => { this.setState({ edit: false }) }}>取消</Button>
+                <Button type="primary" className="save">保存</Button>
+              </div> : 
+              <Button type="primary" onClick={() => this.setState({ edit: true })}>编辑</Button>
+            }
+          </div>
         { this.state.showYaml === true &&
         <CheckYaml self={self} showYaml={this.state.showYaml}
         listPSPDetail={this.props.listPSPDetail}
