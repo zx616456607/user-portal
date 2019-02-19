410c79140c22f4546591f52d889b5994b36880f2
zhouhaitao <capslock.zhou@tenxcloud.com>
2019-02-19 16:35:47 +0800

    [KK-2690] 管理与监控-告警设置，检索记录后再停用，列表搜索结果不正确

--- src/components/ManageMonitor/AlarmSettingSource.js
+++ src/components/ManageMonitor/AlarmSettingSource.js
@@ -1001,10 +1001,12 @@ class AlarmSetting extends Component {
         func: () => {
           notifi.close()
           notifi.success('策略启用成功')
-          getSettingList(clusterID, {
-            targetType: 0,
-          })
-
+/*
+                    getSettingList(clusterID, {
+                      targetType: 0,
+                    })
+          */
+          this.handSearch()
           this.disableButton()
         },
         isAsync: true
@@ -1053,9 +1055,12 @@ class AlarmSetting extends Component {
         func: () => {
           notifi.close()
           notifi.success('策略停用成功')
+          this.handSearch()
+/*
           getSettingList(clusterID, {
             targetType: 0,
           })
+*/
           this.disableButton()
         },
         isAsync: true
