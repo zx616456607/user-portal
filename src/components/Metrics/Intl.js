b26471b0aa7246fcd3bea768ac6cbff1adb1519f
zhangtao <zhangtao@tenxcloud.com>
2019-02-27 20:19:06 +0800

    @Fix issue: [KAN-652] 网络中上传/下载请更改为入站/出站 @Desc: - @Jira Link: - @Root Cause: - @Solution： - @Reviewed By： zhangpc

--- src/components/Metrics/Intl.js
+++ src/components/Metrics/Intl.js
@@ -34,8 +34,8 @@ export const mapData = {
     memory: '内存',
     // Network.js
     network: '网络',
-    download: '下载',
-    upload: '上传',
+    download: '出站',
+    upload: '入站',
     // Disk.js
     disk: '磁盘',
     read: '读取',
