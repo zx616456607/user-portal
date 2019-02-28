590fd67e925770e3dc7239d26834ccd6485f9e1d
zhangtao <zhangtao@tenxcloud.com>
2019-02-28 12:02:23 +0800

    @Fix issue: [KAN-627] 【dev-flows-portal】嵌入 user-portal 修复代码冲突 @Desc: - @Jira Link: - @Root Cause: - @Solution： - @Reviewed By： zhangpc

--- routes/index.js
+++ routes/index.js
@@ -25,8 +25,6 @@ module.exports = function (Router) {
   router.get(/^(\/database_cache|\/database_cache\/[a-zA-Z0-9_-]+|\/database_cache\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
   router.get(/^(\/container|\/container\/[a-zA-Z0-9_-]+|\/container\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
   router.get(/^(\/backup|\/backup\/[a-zA-Z0-9_-]+|\/backup\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
-  router.get(/^(\/devops|\/devops\/[a-zA-Z0-9_-]+|\/devops\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+|\/devops\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+|\/devops\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
-  router.get(/^(\/ai\-deep\-learning|\/ai\-deep\-learning\/[a-zA-Z0-9_-]+|\/ai\-deep\-learning\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
   router.get(/^(\/account|\/account\/[a-zA-Z0-9_-]+|\/account\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+|\/account\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
   router.get(/^(\/manange_monitor|\/manange_monitor\/[a-zA-Z0-9_-]+|\/manange_monitor\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
   router.get(/^(\/middleware_center|\/middleware_center\/[a-zA-Z0-9_-]+|\/middleware_center\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)/, indexCtl.index)
@@ -36,6 +34,7 @@ module.exports = function (Router) {
   router.get(/^\/workloads(\/|)/, indexCtl.index)
   router.get(/^\/net\-management(\/|)/, indexCtl.index)
   router.get(/^\/ai\-deep\-learning(\/|)/, indexCtl.index)
+  router.get(/^\/devops(\/|)/, indexCtl.index)
   if (mode === 'enterprise') {
     router.get(/^(\/tenant_manage|\/tenant_manage\/[a-zA-Z0-9_-]+|\/tenant_manage\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+|\/tenant_manage\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
     router.get(/^(\/setting|\/setting\/[a-zA-Z0-9_-]+|\/setting\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+|\/setting\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)(\/|)$/, indexCtl.index)
