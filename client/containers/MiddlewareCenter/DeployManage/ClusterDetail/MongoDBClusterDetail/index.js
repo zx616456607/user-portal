a7cc1de57bbebd56cd0b1702252204fa7177ed9b
zhouhaitao <capslock.zhou@tenxcloud.com>
2019-03-01 18:42:09 +0800

    [KAN-725] 服务目录-部署管理，mongo集群详情-访问方式，集群内实例地址建议将密码隐藏

--- client/containers/MiddlewareCenter/DeployManage/ClusterDetail/MongoDBClusterDetail/index.js
+++ client/containers/MiddlewareCenter/DeployManage/ClusterDetail/MongoDBClusterDetail/index.js
@@ -901,7 +901,7 @@ class VisitTypesComponent extends Component {
     const { projectName, databaseInfo } = this.props
     const { copyStatus } = this.state
     const clusterAdd = [];
-    clusterAdd[0] = `mongodb+srv://<username>:<password>@${databaseInfo.objectMeta.name}-rs0.${projectName}.svc.cluster.local/admin?ReplSet=rs0&ssl=false`
+    clusterAdd[0] = `mongodb+srv://<username>@${databaseInfo.objectMeta.name}-rs0.${projectName}.svc.cluster.local/admin?ReplSet=rs0&ssl=false`
     if (!clusterAdd.length) return '-'
     const domainList = clusterAdd && clusterAdd.map(item => {
       return (
