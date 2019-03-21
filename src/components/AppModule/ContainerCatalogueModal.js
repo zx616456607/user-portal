ce1296a7da618e1015ed842ff96353d2453c54e7
rensiwei <vsion.ren@tenxcloud.com>
2019-03-21 17:30:32 +0800

    [KAN-1327]

--- src/components/AppModule/ContainerCatalogueModal.js
+++ src/components/AppModule/ContainerCatalogueModal.js
@@ -338,48 +338,46 @@ let ContainerCatalogueModal = React.createClass({
         </Col>
       </Row>
       <FormItem
+        className="host_wrapper"
         label={intl.formatMessage(IntlMessage.hostDirectory)}
         key="host_path"
         labelCol={{ span: 5 }}
         wrapperCol={{ span: 17 }}
       >
-
-        <div className='host_wrapper'>
-          <Input
-            addonBefore={this.state.hostDir}
-            placeholder={intl.formatMessage(IntlMessage.pleaseEnter, {
-              item: intl.formatMessage(IntlMessage.hostDirectory),
-              end: '',
-            })}
-            {...getFieldProps('hostPath', {
-              initialValue: undefined,
-              rules: [{
-                validator: (rule, value, callback) => {
-                  if(!value){
-                    return callback(intl.formatMessage(IntlMessage.pleaseEnter, {
-                      item: intl.formatMessage(IntlMessage.hostDirectory),
-                      end: '',
-                    }))
-                  }
-                  if (!PATH_REG.test(value)) {
-                    return callback(intl.formatMessage(IntlMessage.plsEnterCorrectPath))
-                  }
-                  // if (value.lastIndexOf('/') > 0) {
-                  //   return callback('本地存储不支持挂载多级目录')
-                  // }
-                  //const list = cloneDeep(fieldsList)
-                  //list.splice(currentIndex, 1)
-                  //for (let i = 0; i < list.length; i++) {
-                  //  if (value === list[i].hostPath) {
-                  //    return callback('已填写过该路径')
-                  //  }
-                  //}
-                  return callback()
+        <Input
+          addonBefore={this.state.hostDir}
+          placeholder={intl.formatMessage(IntlMessage.pleaseEnter, {
+            item: intl.formatMessage(IntlMessage.hostDirectory),
+            end: '',
+          })}
+          {...getFieldProps('hostPath', {
+            initialValue: undefined,
+            rules: [{
+              validator: (rule, value, callback) => {
+                if(!value){
+                  return callback(intl.formatMessage(IntlMessage.pleaseEnter, {
+                    item: intl.formatMessage(IntlMessage.hostDirectory),
+                    end: '',
+                  }))
                 }
-              }]
-            })}
-          />
-        </div>
+                if (!PATH_REG.test(value)) {
+                  return callback(intl.formatMessage(IntlMessage.plsEnterCorrectPath))
+                }
+                // if (value.lastIndexOf('/') > 0) {
+                //   return callback('本地存储不支持挂载多级目录')
+                // }
+                //const list = cloneDeep(fieldsList)
+                //list.splice(currentIndex, 1)
+                //for (let i = 0; i < list.length; i++) {
+                //  if (value === list[i].hostPath) {
+                //    return callback('已填写过该路径')
+                //  }
+                //}
+                return callback()
+              }
+            }]
+          })}
+        />
       </FormItem>
     </div>
   },
